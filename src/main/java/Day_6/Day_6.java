0f6a92178938d6424231d2e14b5c391a3295fd91
ourie <ourie@ourienb23.ahasw.cz>
2023-10-18 11:00:07 +0200

    init commit

---
+++ src/main/java/Day_6/Day_6.java
@@ -0,0 +1,45 @@
+package Day_6;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day_6 {
+	
+    public static void main(String[] args) {
+		// SECOND Answer
+		System.out.println("this "+Day_6part2.getAnswer());
+		
+// FIRST Answer		
+//		String puzzleInput=PuzzleInput.getPuzzleInput();
+//		
+//		
+//		
+//		char[] puzzlechars = puzzleInput.toCharArray();
+//		ArrayList<String> list = new ArrayList<>();
+//		
+//		int i = 0;
+//        do {
+//		list.add(puzzlechars[i]+"");
+//		i++;
+//        }
+//		while (list.size()<4);
+//		if (areAllElementsDifferent(list)){System.out.println(i);}
+//		
+//		for(i=4;i<puzzleInput.length();i++){
+//			list.remove(0);
+//			list.add(puzzlechars[i]+"");
+//			if (areAllElementsDifferent(list)){System.out.println(i+1);}
+//		}
+//		
+//    }
+//	public static boolean areAllElementsDifferent(List<String> list) {
+//        Set<String> uniqueElements = new HashSet<>(list);
+//
+//        // If the size of the unique elements set is equal to the original list size,
+//        // it means all elements are different.
+//        return uniqueElements.size() == list.size();
+ 
+	}
+}
