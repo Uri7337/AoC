b69819f22c1c91bce25ca07037de757cdb940587
ourie <ourie@ourienb23.ahasw.cz>
2023-10-20 16:11:45 +0200

    Day_8 progress

--- src/main/java/Day_8/Day_8.java
+++ src/main/java/Day_8/Day_8.java
@@ -1,7 +1,12 @@
+
 package Day_8;
 
+import java.io.IOException;
+
 public class Day_8 {
-    public static void main (String[] args) {
-        System.out.println("hey Day_8!");
+    public static void main (String[] args) throws IOException {
+		Solution_1 s1 = new Solution_1();
+		
+		System.out.println("solution: "+s1.getSolution());
     }
 }
