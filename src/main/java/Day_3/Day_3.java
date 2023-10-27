9832628b634a0eb5ef039e52c6adda73e022b896
ourie <ourie@ourienb23.ahasw.cz>
2023-10-27 11:53:54 +0200

    Day_3 progress

--- src/main/java/Day_3/Day_3.java
+++ src/main/java/Day_3/Day_3.java
@@ -1,7 +1,11 @@
 package Day_3;
 
+import java.io.IOException;
+
 public class Day_3 {
-    public static void main (String[] args) {
-        System.out.println("hey Day_3!");
+    public static void main (String[] args) throws IOException {
+        Solution s = new Solution();
+		
+		s.getSolution();
     }
 }
---
+++ src/main/java/Day_3/Solution.java
@@ -0,0 +1,21 @@
+
+package Day_3;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class Solution {
+
+	public int getSolution() throws IOException {
+		int s = 0;
+		ReadFile rf = new ReadFile();
+		
+		ArrayList<ArrayList<Character>> bigAL = rf.getBigAL(); 
+		
+         
+    
+		return s;
+	}
+
+}
