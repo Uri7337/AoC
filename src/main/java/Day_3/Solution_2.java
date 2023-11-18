9359cf08453c0da188140c881a3101ebd6855c19
Uri7337 <ondrasekuriasek@gmail.com>
2023-11-18 21:39:42 +0100

    solution 2 for day 3

--- src/main/java/Day_3/Solution_2.java
+++ src/main/java/Day_3/Solution_2.java
@@ -2,53 +2,74 @@ package Day_3;
 
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Set;
 
+//vJrwpWtwJgWrhcsFMMfFFhFp
+//jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+//PmmdzqPrVvPwwTWBwg 
 public class Solution_2 {
 
     public int getSolution() {
 
-        boolean found = false;
-        String part1 = "";
-        String part2 = "";
-        int result = 0;
-        int result1 = 0;
-        int result2 = 0;
+        String s = "C:/UriStuff/Other/puzzle.txt";
+        String line1 = "";
+        String line2 = "";
+        String line3 = "";
 
-        String s = "/home/deadpool7337/UriPrograms/Other/PuzzleInputs/Day_3.txt";
+        //ArrayList<Character> matches = new ArrayList<Character>();
+        int result = 0;
+        int lines = 0;
+
+        boolean found = false;
+
         try {
             File myObj = new File(s);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                part1 = data.substring(0, (data.length() / 2));
-                part2 = data.substring((data.length() / 2), data.length());
-                char[] chars1 = part1.toCharArray();
-                char[] chars2 = part2.toCharArray();
-                found = false;
+                line1 = myReader.nextLine();
+                line2 = myReader.nextLine();
+                line3 = myReader.nextLine();
+
+                char[] chars1 = line1.toCharArray();
+                char[] chars2 = line2.toCharArray();
+                char[] chars3 = line3.toCharArray();
+
+                ArrayList<Character> matches = new ArrayList<Character>();
+
                 for (int i = 0; i < chars1.length; i++) {
                     for (int j = 0; j < chars2.length; j++) {
-                        if (!found) {
-                            if (chars1[i] == chars2[j]) {
-                                found = true;
-                                
-                                if (Character.isUpperCase(chars1[i])) {
-                                    int value = chars1[i];
-                                    value -= 38;
-                                   
-                                    result += value;
-                                    break;
-                                } else {
-                                    int value = chars1[i];
-                                    value -= 96;
-                                    
-                                    result += value;
-                                    break;
-                                }
+
+                        if (chars1[i] == chars2[j]) {
+                            matches.add(chars1[i]);
+                        }
+                    }
+                }
+                
+                Set<Character> set = new HashSet<>(matches);
+                matches.clear();
+                matches.addAll(set);
+               
+                for (Character ch : matches) {
+                    for (int i = 0; i < chars3.length; i++) {
+                        if (ch == chars3[i]) {
+                            if (Character.isUpperCase(chars3[i])) {
+                                int value = chars3[i];
+                                value -= 38;
+                                result += value;
+                                break;
+                            } else {
+                                int value = chars3[i];
+                                value -= 96;
+                                result += value;
+                                break;
                             }
                         }
                     }
                 }
+
             }
             myReader.close();
         } catch (FileNotFoundException e) {
