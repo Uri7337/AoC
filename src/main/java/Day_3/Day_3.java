26caa296596fcd6844fa5c168a3ad30c77c3f96d
Uri7337 <ondrasekuriasek@gmail.com>
2023-11-18 23:02:52 +0100

    day_3 main standart

--- src/main/java/Day_3/Day_3.java
+++ src/main/java/Day_3/Day_3.java
@@ -5,8 +5,10 @@ package Day_3;
 
 public class Day_3 {
     public static void main (String[] args) {
-        Solution_1 s = new Solution_1();
+        Solution_1 s1 = new Solution_1();
+        Solution_2 s2 = new Solution_2();
         System.out.println("");
-        System.out.println("Solution: " + s.getSolution());
+        System.out.println("Solution1: " + s1.getSolution());
+        System.out.println("Solution2: " + s2.getSolution());
     }
 }
