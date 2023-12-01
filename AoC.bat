2b44ff238ab016f61eaa3baf2b15d237b0979ddf
Uri7337 <109479378+Uri7337@users.noreply.github.com>
2023-12-01 09:40:49 +0100

    changing AoC.bat
    creating 2 files solution and solution 2 with prep code and i changed day_x.java code and added rems to the bat so its clear what it is for

--- AoC.bat
+++ AoC.bat
@@ -1,3 +1,6 @@
+REM bat file for creating AoC project structure.
+REM Insert bat file at the very top of the project directory and let the magic happend.
+ 
 @echo off
 for /l %%i in (1, 1, 25) do (
     md "Day_%%i"
@@ -7,11 +10,52 @@ for /l %%i in (1, 1, 25) do (
 pushd "%~dp0"
 for /d %%a in (*Day_*) do (
     cd "%%a"
-	echo public class %%a {> %%~nxa.java
+	
+	echo //> %%~nxa.java
+	echo package %%a;>> %%~nxa.java
+	echo //>> %%~nxa.java
+	echo public class %%a {>> %%~nxa.java
 	echo     public static void main ^(String[] args^) {>> %%~nxa.java
-	echo         System.out.println^("hey %%a!"^);>> %%~nxa.java
+	echo         Solution_1_%%a s1 = new Solution_1_%%a^(^);>> %%~nxa.java
+	echo         Solution_2_%%a s2 = new Solution_2_%%a^(^);>> %%~nxa.java
+	echo //>> %%~nxa.java
+	echo		 System.out.println^("%%a_Solution1:" + s1.getSolution^(^)^);>> %%~nxa.java
+	echo		 System.out.println^("%%a_Solution2:" + s2.getSolution^(^)^);>> %%~nxa.java
 	echo     }>> %%~nxa.java
 	echo }>> %%~nxa.java
+	
+	echo package %%a;> Solution_1_%%~nxa.java
+	echo //>> Solution_1_%%~nxa.java
+	echo import Shared.ReadFile;>> Solution_1_%%~nxa.java
+	echo //>> Solution_1_%%~nxa.java
+	echo public class Solution_1_%%a {>> Solution_1_%%~nxa.java
+	echo     int solution = 0;>> Solution_1_%%~nxa.java
+	echo //>> Solution_1_%%~nxa.java
+	echo      public int getSolution^(^) {>> Solution_1_%%~nxa.java
+	echo        ReadFile rf = new ReadFile^(^);>> Solution_1_%%~nxa.java
+	echo        String file = "";>> Solution_1_%%~nxa.java
+	echo        String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";>> Solution_1_%%~nxa.java
+	echo        file = rf.getInput^(filepath^);>> Solution_1_%%~nxa.java       
+	echo        return solution;>> Solution_1_%%~nxa.java
+	echo     }>> Solution_1_%%~nxa.java
+	echo }>> Solution_1_%%~nxa.java
+	
+	echo package %%a;> Solution_2_%%~nxa.java
+	echo //>> Solution_2_%%~nxa.java
+	echo import Shared.ReadFile;>> Solution_2_%%~nxa.java
+	echo //>> Solution_2_%%~nxa.java
+	echo public class Solution_2_%%a {>> Solution_2_%%~nxa.java
+	echo     int solution = 0;>> Solution_2_%%~nxa.java
+	echo //>> Solution_2_%%~nxa.java
+	echo      public int getSolution^(^) {>> Solution_2_%%~nxa.java
+	echo        ReadFile rf = new ReadFile^(^);>> Solution_2_%%~nxa.java
+	echo        String file = "";>> Solution_2_%%~nxa.java
+	echo        String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";>> Solution_2_%%~nxa.java
+	echo        file = rf.getInput^(filepath^);>> Solution_2_%%~nxa.java       
+	echo        return solution;>> Solution_2_%%~nxa.java
+	echo     }>> Solution_2_%%~nxa.java
+	echo }>> Solution_2_%%~nxa.java
+	
     cd ..
 popd
 )
