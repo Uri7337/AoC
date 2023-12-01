58690c6f9fb3a60bc15f022df77642dabe98b568
ourie <ourie@ourienb23.ahasw.cz>
2023-12-01 09:45:05 +0100

    progress on day 1

--- src/main/java/Day_1/Solution_1_Day_1.java
+++ src/main/java/Day_1/Solution_1_Day_1.java
@@ -1,15 +1,22 @@
+
 package Day_1;
-//
+
 import Shared.ReadFile;
-//
+
 public class Solution_1_Day_1 {
-    int solution = 0;
-//
-     public int getSolution() {
-       ReadFile rf = new ReadFile();
-       String file = "";
-       String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
-       file = rf.getInput(filepath);       
-       return solution;
+	int solution = 0;
+	
+    public int getSolution() {
+        ReadFile rf = new ReadFile();
+        String file = "";
+        String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
+        file = rf.getInput(filepath);       
+        String[] commands = file.split("  ");
+        System.out.println(commands[1]);
+        
+			String str="sdfvsdf68fsdfsf8999fsdf09";
+			String numberOnly= str.replaceAll("[^0-9]", "");
+        
+        return solution;
     }
 }
