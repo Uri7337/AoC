c96256d2f6c56f640944379a913111cbfc62e050
deadpool7337 <deadpool7337@deadpool7337-IdeaPad-Slim-5-14ABR8>
2023-12-03 20:05:51 +0100

    Completed day 1

--- src/main/java/Day_1/Solution_1_Day_1.java
+++ src/main/java/Day_1/Solution_1_Day_1.java
@@ -11,16 +11,33 @@ public class Solution_1_Day_1 {
         ReadFile rf = new ReadFile();
         ArrayList<String> data = new ArrayList<String>();
 
-        String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
+        String first = "";
+        String second = "";
+        int number = 0;
+        
+        String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInputs/Day_1.txt";
 
         data = rf.getInput(filepath);
 
         for (int i = 0; i < data.size(); i++) {
             String numbers = data.get(i).replaceAll("[^0-9]", "");
+           
+            first = numbers.substring(0, 1);
+            
+            if (numbers.length() != 1) {
+                
+                second = numbers.substring(numbers.length() - 1);
+                
+            }else{
+                
+                second = first;
+            }
+            
+            numbers = first.concat(second);
             
             try {
-                int number = Integer.parseInt(numbers);
-                System.out.println(number); 
+                number = Integer.parseInt(numbers);
+                solution += number;
             } catch (NumberFormatException ex) {
                 ex.printStackTrace();
             }
--- src/main/java/Day_1/Solution_2_Day_1.java
+++ src/main/java/Day_1/Solution_2_Day_1.java
@@ -1,16 +1,92 @@
 package Day_1;
-//
+
 import Shared.ReadFile;
 import java.util.ArrayList;
-//
+import java.util.TreeMap;
+import java.util.HashMap;
+
 public class Solution_2_Day_1 {
+
     int solution = 0;
-//
-     public int getSolution() {
-       ReadFile rf = new ReadFile();
-       ArrayList<String> file = new ArrayList<String>();
-       String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
-       file = rf.getInput(filepath);       
-       return solution;
+
+    public int getSolution() {
+        ReadFile rf = new ReadFile();
+        ArrayList<String> data = new ArrayList<String>();
+
+        String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInputs/Day_1.txt";
+        data = rf.getInput(filepath);
+
+        HashMap<Integer, String> numberWordMap = new HashMap<>();
+
+        numberWordMap.put(0, "zero");
+        numberWordMap.put(1, "one");
+        numberWordMap.put(2, "two");
+        numberWordMap.put(3, "three");
+        numberWordMap.put(4, "four");
+        numberWordMap.put(5, "five");
+        numberWordMap.put(6, "six");
+        numberWordMap.put(7, "seven");
+        numberWordMap.put(8, "eight");
+        numberWordMap.put(9, "nine");
+
+        for (int i = 0; i < data.size(); i++) {
+            String numbers = "";
+            String first = "";
+            String second = "";
+            int number = 0;
+
+            String puzzleString = data.get(i);
+            System.out.println(puzzleString);
+            TreeMap<Integer, Integer> numbersInString = new TreeMap<>();
+
+            for (Integer key : numberWordMap.keySet()) {
+                int index = puzzleString.indexOf(numberWordMap.get(key));
+                if (index == -1) {
+
+                } else {
+                    while (index != -1) {
+                        numbersInString.put(index, key);
+                        index = puzzleString.indexOf(numberWordMap.get(key), index + 1);
+                    }
+                    System.out.println(numbersInString);
+                }
+            }
+            for (int j = 0; j < puzzleString.length(); j++) {
+                if (Character.isDigit(puzzleString.charAt(j))) {
+                    numbersInString.put(j, Integer.parseInt(String.valueOf(puzzleString.charAt(j))));
+                    System.out.println(numbersInString);
+                }
+            }
+
+            for (Integer k : numbersInString.values()) {
+                numbers += k;
+
+            }
+
+//            
+//           
+            first = numbers.substring(0, 1);
+
+            if (numbers.length() != 1) {
+
+                second = numbers.substring(numbers.length() - 1);
+
+            } else {
+
+                second = first;
+            }
+
+            numbers = first.concat(second);
+
+            try {
+                number = Integer.parseInt(numbers);
+                solution += number;
+                System.out.println(number);
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return solution;
     }
 }
