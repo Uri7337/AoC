e866595330b4657f6d70f6c264fec021fa0b2326
ourie <ourie@ourienb23.ahasw.cz>
2023-12-06 10:40:28 +0100

    finished day 6

--- src/main/java/Day_6/Solution_2_Day_6.java
+++ src/main/java/Day_6/Solution_2_Day_6.java
@@ -1,16 +1,50 @@
 package Day_6;
 //
-import Shared.ReadFile;
+
 import java.util.ArrayList;
+
+
 //
+
+/*
+
+Speed = distance/time = 3meters/3sec = 1m/s | d=s*t, t=d/v
+
+*/
+
 public class Solution_2_Day_6 {
-    int solution = 0;
+    int solution = 1;
+	ArrayList<Integer> s;
 //
      public int getSolution() {
-       ReadFile rf = new ReadFile();
-       ArrayList<String> file = new ArrayList<String>();
-       String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
-       file = rf.getInput(filepath);       
+            
+	   
+	   int[] test_time = {7,15,30};
+	   int[] test_distanceRecord = {9,40,200};
+	   
+	   long time = 40709879L;
+	   long distanceRecord = 215105121471005L;
+	   
+	   long distance = 0L;
+	   long timeleft = 0L;
+	      
+	  
+		   s = new ArrayList<Integer>();
+		   for (int j = 0; j < time; j++) {
+		   timeleft = time - j;
+		   distance = j*timeleft;
+//		   System.out.println("timeleft "+timeleft);
+//		   System.out.println("distance "+distance);
+//		   System.out.println("time "+time[i]);
+		   
+			if(time<distance && distanceRecord<distance){
+				s.add(j);
+			}
+		   }
+		   solution = s.size();
+		 
+	   
+	   
        return solution;
     }
 }
