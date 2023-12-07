079e55f22f4dbf65d2152833371a6bdc1ea0aaf5
ourie <ourie@ourienb23.ahasw.cz>
2023-12-07 09:32:51 +0100

    progress on day 7

--- src/main/java/Day_7/Day_7.java
+++ src/main/java/Day_7/Day_7.java
@@ -1,12 +1,12 @@
-//
+
 package Day_7;
-//
+
 public class Day_7 {
     public static void main (String[] args) {
         Solution_1_Day_7 s1 = new Solution_1_Day_7();
-        Solution_2_Day_7 s2 = new Solution_2_Day_7();
-//
+//        Solution_2_Day_7 s2 = new Solution_2_Day_7();
+
 	 System.out.println("Day_7_Solution1:" + s1.getSolution());
-	 System.out.println("Day_7_Solution2:" + s2.getSolution());
+//	 System.out.println("Day_7_Solution2:" + s2.getSolution());
     }
 }
