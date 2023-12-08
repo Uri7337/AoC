7b18dbbfbb27b45ee319462a3a799ed97929e6e0
ourie <ourie@ourienb23.ahasw.cz>
2023-12-08 11:01:42 +0100

    day 8 progress

---
+++ src/main/java/Day_8/Element.java
@@ -0,0 +1,14 @@
+
+package Day_8;
+
+public class Element {
+	String name= "";
+	String left= "";
+	String right= "";
+
+	public Element(String name, String left, String right) {
+		this.name=name;
+		this.left=left;
+		this.right=right;
+	}
+}
