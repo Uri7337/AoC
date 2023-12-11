9e1e128bccb9de0ff3bd38cea61471caa9f5eb07
ourie <ourie@ourienb23.ahasw.cz>
2023-12-11 10:43:22 +0100

    finished day 8 solution 1

--- src/main/java/Day_8/Solution_1_Day_8.java
+++ src/main/java/Day_8/Solution_1_Day_8.java
@@ -14,7 +14,7 @@ public class Solution_1_Day_8 {
 		String filepath = "C:\\ProgramsUri\\ALL_Files\\Temp_WorkSpace\\Day_8.txt";
 		file = rf.getInput(filepath);
 
-		String input = "LLR";
+		String input = "LRRLRRRLRRLLLRLLRRLRRLLRRRLRRLLRLRRRLRLRRLRLRRRLRLRLRRLLRLRLRRLRRRLRRRLRRRLRLRRLLLLRLLRLLRRLRRRLLLRLRRRLRLRRRLRLRRLRRRLRRRLRLRLLRRRLLRLLRLRLRLRLLRRLRRLRRRLRRLRLRLRLRLRRLRRRLLRRRLLRLLLRRRLLRRRLRRRLRRLRLRRLRLLRRLLRRLRLRLRRLRLRRLLRRRLLRRRLLRLRRRLRLRRRLRLRRRLRRRLRRLRRLRRLLRRRLRRRLLLRRRR";
 
 		for (int i = 0; i < file.size(); i++) {
 			String[] separated = file.get(i).split("[ =(,)]+");
@@ -39,33 +39,34 @@ public class Solution_1_Day_8 {
 		boolean foundit = false;
 
 		while (!foundit) {
-			if (inputloop > inputarr.length) {
+			if (inputloop >= inputarr.length) {
 				inputloop = 0;
 			}
-			System.out.println("inputarr.length: "+inputarr.length);
-			System.out.println("inputloop: "+inputloop);
-			System.out.println("inputarr[inputloop]: "+inputarr[inputloop]);
+//			System.out.println("inputarr.length: "+inputarr.length);
+//			System.out.println("inputloop: "+inputloop);
+//			System.out.println("inputarr[inputloop]: "+inputarr[inputloop]);
 			if ("L".equals(inputarr[inputloop])) {
 				for (int j = 0; j < element.size(); j++) {
 					if (step.equals(element.get(j).name)) {
-						System.out.println("step: "+step);
-						System.out.println("jleft: "+j);
-						System.out.println("element.get(j).name: "+element.get(j).name);
-						System.out.println("element.get(j).left: "+element.get(j).left);
+//						System.out.println("step: "+step);
+//						System.out.println("jleft: "+j);
+//						System.out.println("element.get(j).name: "+element.get(j).name);
+//						System.out.println("element.get(j).left: "+element.get(j).left);
 						step = element.get(j).left;
-						System.out.println("step2: "+step);
-
+//						System.out.println("step: "+step);
+						break;
 					}
 				}
 			} else {
 				for (int j = 0; j < element.size(); j++) {
 					if (step.equals(element.get(j).name)) {
-						System.out.println("step: "+step);
-						System.out.println("jright: "+j);
-						System.out.println("element.get(j).name: "+element.get(j).name);
-						System.out.println("element.get(j).right: "+element.get(j).right);
+//						System.out.println("step: "+step);
+//						System.out.println("jright: "+j);
+//						System.out.println("element.get(j).name: "+element.get(j).name);
+//						System.out.println("element.get(j).right: "+element.get(j).right);
 						step = element.get(j).right;
-						System.out.println("step2: "+step);
+//						System.out.println("step2: "+step);
+						break;
 					}
 				}
 			}
@@ -81,6 +82,6 @@ public class Solution_1_Day_8 {
 			inputloop++;
 			solution++;
 		}
-		return solution;
+		return solution+1;
 	}
 }
