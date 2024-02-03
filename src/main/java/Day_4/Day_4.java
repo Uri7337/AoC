b153ce553f83ffd8e4e617a02ac03f45de801683
deadpool7337 <deadpool7337@deadpool7337-IdeaPad-Slim-5-14ABR8>
2024-02-03 19:25:49 +0100

    Day 4 Completed

--- src/main/java/Day_4/Day_4.java
+++ src/main/java/Day_4/Day_4.java
@@ -1,12 +1,14 @@
-//
 package Day_4;
-//
+
 public class Day_4 {
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Solution_1_Day_4 s1 = new Solution_1_Day_4();
         Solution_2_Day_4 s2 = new Solution_2_Day_4();
-//
-	 System.out.println("Day_4_Solution1:" + s1.getSolution());
-	 System.out.println("Day_4_Solution2:" + s2.getSolution());
+
+        //Change for wanted solution
+        boolean Solution1 = true;
+//        boolean Solution1 = false;
+        System.out.println(Solution1 ? "Day_4_Solution1: " + s1.getSolution() : "Day_4_Solution2: " + s2.getSolution());
     }
 }
--- src/main/java/Day_4/Solution_1_Day_4.java
+++ src/main/java/Day_4/Solution_1_Day_4.java
@@ -2,6 +2,9 @@ package Day_4;
 //
 import Shared.ReadFile;
 import java.util.ArrayList;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 //
 public class Solution_1_Day_4 {
     int solution = 0;
@@ -9,8 +12,60 @@ public class Solution_1_Day_4 {
      public int getSolution() {
        ReadFile rf = new ReadFile();
        ArrayList<String> file = new ArrayList<String>();
-       String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
-       file = rf.getInput(filepath);       
+//       String filepath = "/home/deadpool7337/UriPrograms/Other/AoC/PuzzleInputs/Day_4_2015.txt";
+       String filepath = "/home/deadpool7337/UriPrograms/Other/AoC/PuzzleInputs/Day_4_test_2015.txt";
+       file = rf.getInput(filepath);
+       
+       String s = file.get(0);
+       
+       int i = 0;
+       while(true){
+           String hash = getMd5(s+i);
+           
+//           System.out.println("i:" + i + " h: " + hash);
+           
+           if (hash.startsWith("00000")){
+               solution = i;
+               System.out.println("FINAL = "+"i:" + i + " h: " + hash);
+               break;
+           }
+           
+           
+        i++;   
+       }
+       
+        
+       
+       
+       
        return solution;
+    }
+     
+    public static String getMd5(String input)
+    {
+        try {
+ 
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+ 
+            // digest() method is called to calculate message digest
+            // of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+ 
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+ 
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+ 
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
