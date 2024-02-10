b041650c1b7c372609d4ac08de5c76a15ecd8628
deadpool7337 <deadpool7337@deadpool7337-IdeaPad-Slim-5-14ABR8>
2024-02-10 22:30:34 +0100

    Day 5 Solution 2 Progress

--- src/main/java/Day_5/Solution_2_Day_5.java
+++ src/main/java/Day_5/Solution_2_Day_5.java
@@ -1,6 +1,7 @@
 package Day_5;
 
 import Shared.ReadFile;
+import com.sun.tools.javac.util.StringUtils;
 import java.util.ArrayList;
 
 public class Solution_2_Day_5 {
@@ -11,32 +12,34 @@ public class Solution_2_Day_5 {
     public int getSolution() {
         ReadFile rf = new ReadFile();
 
-        String filepath = "/home/deadpool7337/UriPrograms/Other/AoC/PuzzleInputs/Day_5_test_2015.txt";
-//        String filepath = "/home/deadpool7337/UriPrograms/Other/AoC/PuzzleInputs/Day_5_2015.txt";
+//        String filepath = "/home/deadpool7337/UriPrograms/Other/AoC/PuzzleInputs/Day_5_test_2015.txt";
+        String filepath = "/home/deadpool7337/UriPrograms/Other/AoC/PuzzleInputs/Day_5_2015.txt";
         file = rf.getInput(filepath);
 
         //Insert Solution Here:
         for (int row = 0; row < file.size(); row++) {
 
+            System.out.println("------");
             System.out.println(file.get(row));
-            System.out.println(pair(row));
-//            System.out.println(repeats(row));
+            System.out.println("pair: "+pair(row));
+            System.out.println("repeats: "+repeats(row));
+            System.out.println("------");
+            
+            if (pair(row) && repeats(row)) {
+                solution++;
 
-            
-                if (pair(row) && repeats(row)) {
-                    solution++;
-                }
-            
+            }
 
         }
 
         return solution;
     }
-    
-    public boolean pair(int filerow) {
-        char currletter = ' ';
-        char currbroletter = ' ';
 
+    public boolean pair(int filerow) {
+        char gfch = ' '; //guessFirstChar
+        char gsch = ' '; //guessSecondChar
+
+<<<<<<< HEAD
         for (int i = 0; i < file.get(filerow).length(); i++) {
             
             currletter = file.get(filerow).charAt(i);
@@ -45,33 +48,63 @@ public class Solution_2_Day_5 {
             System.out.println(pair);
             int ind = file.get(filerow).indexOf(pair,i);
             if (ind>=0) {
-                return true;
+=======
+        for (int i = 0; i < file.get(filerow).length()-2; i++) {
+            ArrayList<Integer> indexarr = new ArrayList<Integer>();
+            String word = file.get(filerow);
+            gfch = word.charAt(i);
+            gsch = word.charAt(i + 1);
 
+            String guess = gfch + "" + gsch;
+
+            int index = word.indexOf(guess);
+            indexarr.add(index);
+            
+            while (index >= 0) {
+                index = word.indexOf(guess, index + 1);
+                
+                indexarr.add(index);
+                
+            }
+            
+            int firstloc = indexarr.get(0)+1;
+            int secondloc = indexarr.get(1);
+            
+            if(indexarr.size()==3 && firstloc != secondloc){
+                System.out.println(indexarr);
+>>>>>>> 246bc3c (Day 5 Solution 2 Progress)
+                return true;
             }
         }
         return false;
     }
-    
+
     public boolean repeats(int filerow) {
-        char currletter = ' ';
-        char before = ' ';
-        char after = ' ';
+        char cl = ' '; //currletter
+        char csl = ' '; //currsameletter
 
-        for (int i = 0; i < file.get(filerow).length(); i++) {
+        for (int i = 0; i < (file.get(filerow).length() - 3); i++) {
+            String input = file.get(filerow);
+            cl = input.charAt(i);
 
-            currletter = file.get(filerow).charAt(i);
+            csl = input.charAt(i + 2);
 
+<<<<<<< HEAD
             before = i == 0 ? ' ' : file.get(filerow).charAt(i - 1);
             after = i == (file.get(filerow).length() - 1) ? ' ' : file.get(filerow).charAt(i + 1);
             
             int comparebefore = Character.compare(currletter, before);
             int compareafter = Character.compare(currletter, after);
             if (comparebefore == 0 || compareafter == 0) {
+=======
+            if (cl == csl) {
+                System.out.println(cl+""+input.charAt(i + 1)+""+csl);
+>>>>>>> 246bc3c (Day 5 Solution 2 Progress)
                 return true;
-
             }
+
         }
         return false;
     }
-    
+
 }
