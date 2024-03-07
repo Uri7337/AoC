fcbdc14d268c18cc9f5eee8784ddb355ac5dd61e
ourie <ourie@ourienb23.ahasw.cz>
2024-03-07 08:05:50 +0100

    Day 7 part one progress

--- src/main/java/Day_07/Solution_1_Day_07.java
+++ src/main/java/Day_07/Solution_1_Day_07.java
@@ -15,35 +15,48 @@ public class Solution_1_Day_07 {
 //		String filepath = "./src/main/resources/Day_07_test_2015.txt";
 		String filepath = "./src/main/resources/Day_07_2015.txt";
 		file = rf.getInput(filepath);
-
+		
 		
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
 			String[] lineparts = line.split(" ");
 			
+			int value = 0;
+			
 			//NOT
 			if(lineparts[0].equals("NOT")){
-				int value = hshmp.get(lineparts[1]);
+				if((hshmp.get(lineparts[1]) == null)){
+					hshmp.put(lineparts[1], 0);
+				}
+				value = hshmp.get(lineparts[1]);
 				value = ~value;
 				value = 65536 + value;
 				hshmp.put(lineparts[3], value);
 				
 			//numbers
 			}else if(lineparts[0].matches(".*\\d.*")){
-				int value = Integer.parseInt(lineparts[0]);
+				value = Integer.parseInt(lineparts[0]);
 				hshmp.put(lineparts[2], value);
 				
 			//letters	
 			}else{
 				String gate = lineparts[1];
-				//add if na letters, pokud to nebudou nejake vazby
-				System.out.println(hshmp.get(lineparts[0]));
+				if((hshmp.get(lineparts[0]) == null)){
+					hshmp.put(lineparts[0], 0);
+				}
 				
-				int value1 = hshmp.get(lineparts[0]);
-				int value2 = lineparts[2].matches(".*\\d.*") ? Integer.parseInt(lineparts[2]) : hshmp.get(lineparts[2]);
-				int value = 0;
+				if((hshmp.get(lineparts[2]) == null)){
+					hshmp.put(lineparts[2], 0);
+				}
 				
+				
+				int value1 = 0;
+				int value2 = 0;
+			
+				value1 = hshmp.get(lineparts[0]);
+				value2 = lineparts[2].matches(".*\\d.*") ? Integer.parseInt(lineparts[2]) : hshmp.get(lineparts[2]);
+						
 				switch (gate) {
 					case "AND":
 						value = value1 & value2;
@@ -59,16 +72,25 @@ public class Solution_1_Day_07 {
 						
 						value = value1 >> value2;
 						break;
+					case "->":
+						hshmp.put(lineparts[2], value1);
+						break;
 					default:
 						throw new AssertionError();
 				}
-				hshmp.put(lineparts[4], value);
+				if(4 < lineparts.length){
+					hshmp.put(lineparts[4], value);
+				}else{
+					hshmp.put(lineparts[2], value);
+				}
+				
 				
 			}
 			 
 
 		} 
 		System.out.println(hshmp);
+		solution = hshmp.get("a");
 		return solution;
 	}
 
