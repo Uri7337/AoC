79529b3ef4af62f5769ff766e713e9ccf4cdb099
ourie <ourie@ourienb23.ahasw.cz>
2024-03-30 07:32:04 +0100

    Day 8 sol_1

--- src/main/java/Day_08/Solution_1_Day_08.java
+++ src/main/java/Day_08/Solution_1_Day_08.java
@@ -16,51 +16,62 @@ public class Solution_1_Day_08 {
 
 	int solution = 0;
 	HashMap<String, Integer> hshmp = new HashMap<String, Integer>();
-	
-	
+
 	public int getSolution() {
 		ReadFile rf = new ReadFile();
 		ArrayList<String> file = new ArrayList<String>();
-		String filepath = "./src/main/resources/Day_08_test_2015.txt";
-//		String filepath = "./src/main/resources/Day_08_2015.txt";
+//		String filepath = "./src/main/resources/Day_08_test_2015.txt";
+		String filepath = "./src/main/resources/Day_08_2015.txt";
 		file = rf.getInput(filepath);
-		
+
 		int total_code = 0;
 		int total_string = 0;
-		
+
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
-			
-			int code_sol = line.length();  
-			int string_sol = code_sol-2; 
-			
-			
-			
-			String[] groups = line.split("\\");
-			int j = 0;
-			for(String group : groups){
-				System.out.println(j);
-				j++;
-				System.out.println(group);
+
+			int code_sol = line.length();
+			int string_sol = code_sol - 2;
+
+			int index = line.indexOf('\\');
+			while (index >= 0) {
+				string_sol = calcLineVal(line, index, string_sol);
+				index = line.indexOf('\\', index + 2);
 			}
+
 			
-			
-			
-			printInfo(code_sol,string_sol,line);
-			
+
+			printInfo(code_sol, string_sol, line);
 			total_code = total_code + code_sol;
 			total_string = total_string + string_sol;
 		}
-		
 
 		solution = total_code - total_string;
-		
+
 		return solution;
 	}
-	public void printInfo(int c, int s, String line){
-		System.out.print("c:"+c + "|");
-		System.out.print("s:"+s + " ");
+
+	public void printInfo(int c, int s, String line) {
+		System.out.print("c:" + c + "|");
+		System.out.print("s:" + s + " ");
 		System.out.println(line);
+	}
+
+	public int calcLineVal(String line, int index, int string_sol) {
+		switch (line.charAt(index + 1)) {
+			case '\\':
+				string_sol--;
+				break;
+			case '\"':
+				string_sol--;
+				break;
+			case 'x':
+				string_sol = string_sol - 3;
+				break;
+			default:
+				throw new AssertionError();
+		}
+		return string_sol;
 	}
 }
