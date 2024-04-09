e99dbc0ea9121f794fe78a2a39076cd415a9cd57
ourie <ourie@ourienb23.ahasw.cz>
2024-04-09 08:06:39 +0200

    Day 10 prep

--- src/main/java/Day_10/Day_10.java
+++ src/main/java/Day_10/Day_10.java
@@ -1,12 +1,14 @@
-//
 package Day_10;
-//
+
 public class Day_10 {
-    public static void main (String[] args) {
-        Solution_1_Day_10 s1 = new Solution_1_Day_10();
-        Solution_2_Day_10 s2 = new Solution_2_Day_10();
-//
-	 System.out.println("Day_10_Solution1:" + s1.getSolution());
-	 System.out.println("Day_10_Solution2:" + s2.getSolution());
-    }
+	public static void main (String[] args) {
+		Solution_1_Day_10 s1 = new Solution_1_Day_10();
+		Solution_2_Day_10 s2 = new Solution_2_Day_10();
+
+		//Change for wanted solution TRUE for SOL1
+		boolean Solution1 = true;
+//		boolean Solution1 = false;
+		System.out.println(Solution1 ? "Day_10_Solution1: " + s1.getSolution() : "Day_10_Solution2: " + s2.getSolution());
+	}
 }
+
--- src/main/java/Day_10/Solution_2_Day_10.java
+++ src/main/java/Day_10/Solution_2_Day_10.java
@@ -1,16 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
 package Day_10;
-//
+
 import Shared.ReadFile;
+
 import java.util.ArrayList;
-//
+
+
+/**
+ *
+ * @author ourie
+ */
+
+
 public class Solution_2_Day_10 {
-    int solution = 0;
-//
-     public int getSolution() {
-       ReadFile rf = new ReadFile();
-       ArrayList<String> file = new ArrayList<String>();
-       String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
-       file = rf.getInput(filepath);       
-       return solution;
-    }
+
+	int solution = 0;
+
+	
+	public int getSolution() {
+		ReadFile rf = new ReadFile();
+		ArrayList<String> file = new ArrayList<String>();
+//		String filepath = "./src/main/resources/Day_10_test_2015.txt";
+		String filepath = "./src/main/resources/Day_10_2015.txt";
+		file = rf.getInput(filepath);
+		
+		
+		
+		//Insert Solution Here:
+		for (int i = 0; i < file.size(); i++) {
+			String[] line_parts = file.get(i).split(" ");
+
+			
+		}
+		
+		
+		
+		solution = 0;
+		
+		return solution;
+	}
+	
+	
+
+	
+	
+	
 }
