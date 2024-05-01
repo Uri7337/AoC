68097e676f1b24abcb017fd40655e8a62cb160de
deadpool7337 <deadpool7337@deadpool7337-IdeaPad-Slim-5-14ABR8>
2024-05-01 10:32:43 +0200

    Day 11 prep

--- src/main/java/Day_11/Day_11.java
+++ src/main/java/Day_11/Day_11.java
@@ -1,12 +1,14 @@
-//
 package Day_11;
-//
+
 public class Day_11 {
-    public static void main (String[] args) {
-        Solution_1_Day_11 s1 = new Solution_1_Day_11();
-        Solution_2_Day_11 s2 = new Solution_2_Day_11();
-//
-	 System.out.println("Day_11_Solution1:" + s1.getSolution());
-	 System.out.println("Day_11_Solution2:" + s2.getSolution());
-    }
+	public static void main (String[] args) {
+		Solution_1_Day_11 s1 = new Solution_1_Day_11();
+		Solution_2_Day_11 s2 = new Solution_2_Day_11();
+
+		//Change for wanted solution TRUE for SOL1
+		boolean Solution1 = true;
+//		boolean Solution1 = false;
+		System.out.println(Solution1 ? "Day_11_Solution1: " + s1.getSolution() : "Day_11_Solution2: " + s2.getSolution());
+	}
 }
+
--- src/main/java/Day_11/Solution_1_Day_11.java
+++ src/main/java/Day_11/Solution_1_Day_11.java
@@ -1,16 +1,27 @@
 package Day_11;
-//
+
 import Shared.ReadFile;
+
 import java.util.ArrayList;
-//
+
 public class Solution_1_Day_11 {
+
     int solution = 0;
-//
-     public int getSolution() {
-       ReadFile rf = new ReadFile();
-       ArrayList<String> file = new ArrayList<String>();
-       String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
-       file = rf.getInput(filepath);       
-       return solution;
+
+    public int getSolution() {
+        ReadFile rf = new ReadFile();
+        ArrayList<String> file = new ArrayList<String>();
+        String filepath = "./src/main/resources/Day_11_test_2015.txt";
+//		String filepath = "./src/main/resources/Day_11_2015.txt";
+        file = rf.getInput(filepath);
+
+        //Insert Solution Here:
+        for (int i = 0; i < file.size(); i++) {
+            String line = file.get(i);
+
+        }
+        solution = 0;
+        return solution;
     }
+
 }
