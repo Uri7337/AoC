f9793782b6fc197181b293ec3f0f3d219064f76e
ourie <ourie@ourienb23.ahasw.cz>
2024-05-02 18:18:14 +0200

    Day 11 small progress

--- src/main/java/Day_11/Solution_1_Day_11.java
+++ src/main/java/Day_11/Solution_1_Day_11.java
@@ -6,22 +6,62 @@ import java.util.ArrayList;
 
 public class Solution_1_Day_11 {
 
-    int solution = 0;
+	int solution = 0;
 
-    public int getSolution() {
-        ReadFile rf = new ReadFile();
-        ArrayList<String> file = new ArrayList<String>();
-        String filepath = "./src/main/resources/Day_11_test_2015.txt";
+	public int getSolution() {
+		ReadFile rf = new ReadFile();
+		ArrayList<String> file = new ArrayList<String>();
+		String filepath = "./src/main/resources/Day_11_test_2015.txt";
 //		String filepath = "./src/main/resources/Day_11_2015.txt";
-        file = rf.getInput(filepath);
+		file = rf.getInput(filepath);
 
-        //Insert Solution Here:
-        for (int i = 0; i < file.size(); i++) {
-            String line = file.get(i);
+		//Insert Solution Here:
+		for (int i = 0; i < file.size(); i++) {
+			String line = file.get(i);
 
-        }
-        solution = 0;
-        return solution;
-    }
+			testStrings(line);
+		}
+		solution = 0;
+		return solution;
+	}
 
+	public void testStrings(String line) {
+		switch (line) {
+			case "hijklmmn":
+				// code block
+				break;
+			case "abbceffg":
+				// code block
+				break;
+				case "abbcegjk":
+				// code block
+				break;
+				case "abcdefgh":
+				// code block
+				break;
+				case "ghijklmn":
+				// code block
+				break;
+			default:
+			// code block
+		}
+	}
+	
+	public void testStraight(String line){
+		boolean straight = Straight(line);
+		if(straight){
+			System.out.println("1. rule: Correct");
+		}else{
+			System.out.println("1. rule: Wrong");
+		}
+		
+	}
+	
+	public boolean Straight(String line){
+		boolean isStraight = false;
+		
+		
+		
+		return isStraight;
+	}
 }
