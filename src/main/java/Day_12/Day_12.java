c0a60615245746cbb523358ae8f149543aa46a98
ourie <ourie@ourienb23.ahasw.cz>
2024-05-05 17:06:38 +0200

    feat: Day Templates feat: Day 11 puzzleinput feat: Day 12 prep

--- src/main/java/Day_12/Day_12.java
+++ src/main/java/Day_12/Day_12.java
@@ -1,12 +1,14 @@
-//
 package Day_12;
-//
+
 public class Day_12 {
-    public static void main (String[] args) {
-        Solution_1_Day_12 s1 = new Solution_1_Day_12();
-        Solution_2_Day_12 s2 = new Solution_2_Day_12();
-//
-	 System.out.println("Day_12_Solution1:" + s1.getSolution());
-	 System.out.println("Day_12_Solution2:" + s2.getSolution());
-    }
+	public static void main (String[] args) {
+		Solution_1_Day_12 s1 = new Solution_1_Day_12();
+		Solution_2_Day_12 s2 = new Solution_2_Day_12();
+
+		//Change for wanted solution TRUE for SOL1
+		boolean Solution1 = true;
+//		boolean Solution1 = false;
+		System.out.println(Solution1 ? "Day_12_Solution1: " + s1.getSolution() : "Day_12_Solution2: " + s2.getSolution());
+	}
 }
+
--- src/main/java/Day_12/Solution_1_Day_12.java
+++ src/main/java/Day_12/Solution_1_Day_12.java
@@ -1,16 +1,37 @@
 package Day_12;
-//
+
 import Shared.ReadFile;
+
 import java.util.ArrayList;
-//
+
 public class Solution_1_Day_12 {
-    int solution = 0;
-//
-     public int getSolution() {
-       ReadFile rf = new ReadFile();
-       ArrayList<String> file = new ArrayList<String>();
-       String filepath = "/home/deadpool7337/UriPrograms/Other/PuzzleInput.txt";
-       file = rf.getInput(filepath);       
-       return solution;
-    }
+
+	int solution = 0;
+
+	public int getSolution() {
+		ReadFile rf = new ReadFile();
+		ArrayList<String> file = new ArrayList<String>();
+		String filepath = "./src/main/resources/Day_12_test_2015.txt";
+//		String filepath = "./src/main/resources/Day_12_2015.txt";
+		file = rf.getInput(filepath);
+
+		//Insert Solution Here:
+		for (int i = 0; i < file.size(); i++) {
+			String line = file.get(i);
+
+			
+		}
+		solution = 0;
+		return solution;
+	}
+	
+
+	//prints content, System.out.println is way too long
+	public void p(String content) {
+		System.out.println(content);
+	}
+	
+	// ----debug zone----
+	
+
 }
