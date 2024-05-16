6a56575c330dde77e6093658483b5ff8781b968d
ourie <ourie@ourienb23.ahasw.cz>
2024-05-16 07:52:59 +0200

    feat: Day 12 progress

--- src/main/java/Day_12/Solution_2_Day_12.java
+++ src/main/java/Day_12/Solution_2_Day_12.java
@@ -4,8 +4,15 @@ import Shared.ReadFile;
 
 import java.util.ArrayList;
 import com.google.gson.Gson;
+import java.util.AbstractMap;
 import java.util.List;
 
+/*
+1. GetClass
+2. instance of - if (jsonObject instanceof List l) {
+3. Přetypování (primitivní typy jsou něco jiného) 
+4. rekurze počítání - count
+ */
 public class Solution_2_Day_12 {
 
 	int solution = 0;
@@ -20,61 +27,53 @@ public class Solution_2_Day_12 {
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
-			String regex = "\\{[^}]*red[^}]*\\}"; 
 			ArrayList<Integer> numbers = new ArrayList<Integer>();
-			
+
 			// Create Gson object
 			Gson gson = new Gson();
 
 			// Convert string to JSON
 			Object jsonObject = gson.fromJson(line, Object.class);
-			
 			if (jsonObject instanceof List) {
 				List jo = (List) jsonObject;
 				for (Object object : jo) {
-					System.out.println(object.getClass());   
+					count(object);
 				}
+			}else if (jsonObject instanceof AbstractMap) {
+				p("mapiška/jakoby objektik in JSON | tak koukneme do mapišky:");
+				AbstractMap obj = (AbstractMap) jsonObject;
+				System.out.println(obj.values());
+				p("zandame mapišku");
 			}
 
-			// Print JSON object
-			System.out.println(jsonObject.getClass());   
-			
-//			//find all red objects and replace them with ""
-//			line = line.replaceAll(regex, "");
-//			
-//			//then change the regex for everything but numbers and clear string again 
-//			regex = "[^-\\d]+"; 
-//			line = line.replaceAll(regex, ",");
-//			
-//			
-//			//there was space infront of the first number.
-//			line = line.substring(1);
-//			
-//			
-//			String[] lineArray = line.split(",");
-//			
-//			for (String linePiece : lineArray){
-//				numbers.add(Integer.parseInt(linePiece));
-//			}
-//			
-//			for (int n :numbers){
-//				solution = solution + n;
-//				
-//			}
-			
 		}
-		
-		
+
 		return solution;
 	}
-	
+
+	public void count(Object object) {
+//		System.out.println(object.getClass());   
+		if (object instanceof AbstractMap) {
+			p("mapiška/jakoby objektik in JSON | tak koukneme do mapišky:");
+			AbstractMap obj = (AbstractMap) object;
+			System.out.println(obj.entrySet());
+			p("zandame mapišku");
+		} else if (object instanceof List) {
+			p("listík/jakoby array in JSON | tak si přečteme listik:");
+			count(object);
+			p("srolujeme listik");
+		} else if (object instanceof Double) {
+			p("cisilko");
+		} else {
+			p("neznam: " + object.getClass());
+		}
+
+	}
 
 	//prints content, System.out.println is way too long
 	public void p(Object content) {
 		System.out.println(content.toString());
 	}
-	
-	// ----debug zone----
-	
 
+	// ----debug zone----
 }
