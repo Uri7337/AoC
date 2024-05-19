7a9a90f70151722b2f57f729d2d924f79f9c5b47
ourie <ourie@ourienb23.ahasw.cz>
2024-05-19 20:46:09 +0200

    feat: Day 12 progress

--- src/main/java/Day_12/Solution_2_Day_12.java
+++ src/main/java/Day_12/Solution_2_Day_12.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import com.google.gson.Gson;
 import java.util.AbstractMap;
 import java.util.List;
+import java.util.Map;
 
 /*
 1. GetClass
@@ -28,7 +29,7 @@ public class Solution_2_Day_12 {
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
 			ArrayList<Integer> numbers = new ArrayList<Integer>();
-
+			int[] res = {0, 0};
 			// Create Gson object
 			Gson gson = new Gson();
 
@@ -37,35 +38,79 @@ public class Solution_2_Day_12 {
 			if (jsonObject instanceof List) {
 				List jo = (List) jsonObject;
 				for (Object object : jo) {
-					count(object);
+					res = count(object);
+					if (res[1] != 1) {
+						solution = solution + res[0];
+					}
 				}
-			}else if (jsonObject instanceof AbstractMap) {
-				p("mapiška/jakoby objektik in JSON | tak koukneme do mapišky:");
-				AbstractMap obj = (AbstractMap) jsonObject;
-				System.out.println(obj.values());
-				p("zandame mapišku");
 			}
+//			else if (jsonObject instanceof AbstractMap) {
+//				p("mapiška/jakoby objektik in JSON | tak koukneme do mapišky:");
+//				AbstractMap obj = (AbstractMap) jsonObject;
+//				System.out.println(obj.values());
+//				p("zandame mapišku");
+//			}
 
 		}
 
 		return solution;
 	}
 
-	public void count(Object object) {
+	public int[] count(Object object) {
+		// ifRedThenOne[1] == 1 if object contains red String;
+		int[] ifRedThenOne = {0, 0};
 //		System.out.println(object.getClass());   
 		if (object instanceof AbstractMap) {
+			AbstractMap map = (AbstractMap) object;
+			int[] mapRes = {0, 0};
+
 			p("mapiška/jakoby objektik in JSON | tak koukneme do mapišky:");
-			AbstractMap obj = (AbstractMap) object;
-			System.out.println(obj.entrySet());
+			System.out.println(map.entrySet());
+
+			for (Object o : map.values()) {
+				mapRes = count(o);
+				if (mapRes[1] == 1) {
+					ifRedThenOne[0] = 0;
+//					ifRedThenOne[0] = 0;
+					return ifRedThenOne;
+				} else {
+					ifRedThenOne[0] = ifRedThenOne[0] + mapRes[0];
+				}
+			}
+
 			p("zandame mapišku");
 		} else if (object instanceof List) {
 			p("listík/jakoby array in JSON | tak si přečteme listik:");
-			count(object);
+			List l = (List) object;
+			for (Object listitem : l) {
+					res[] = count(listitem);
+					if (res[1] != 1) {
+						solution = solution + res[0];
+					}
+				}
+			
 			p("srolujeme listik");
 		} else if (object instanceof Double) {
-			p("cisilko");
+			Double doub = (Double) object;
+			p("cisilko: " + doub.toString());
+			int num = doub.intValue();
+			p("cisilko intik: " + num);
+			ifRedThenOne[0] = ifRedThenOne[0] + num;
+		} else if (object instanceof String) {
+			String s = (String) object;
+			p("stringacek: " + s);
+			if (s.equals("red")) {
+				ifRedThenOne[1] = 1;
+			}
+
 		} else {
 			p("neznam: " + object.getClass());
+		}
+		if (ifRedThenOne[1] == 1) {
+			ifRedThenOne[0] = 0;
+			return ifRedThenOne;
+		} else {
+			return ifRedThenOne;
 		}
 
 	}
