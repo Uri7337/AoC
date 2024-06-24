c28ef91fa79c02f1c3fa3d58dbb32f92a6c7709f
ourie <ourie@ourienb23.ahasw.cz>
2024-06-24 12:17:54 +0200

    Day 1 progress

--- src/main/java/Day_01/Person.java
+++ src/main/java/Day_01/Person.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Person {
+    //facing North 
     int cardinalDirections = 0;
+    //index 0 = X ; index 1 = Y
     ArrayList<Integer> mapLocation = new ArrayList<>(Arrays.asList(0, 0));
 
     // for Example: facing North is 0 
@@ -12,6 +14,13 @@ public class Person {
 
 		return cardinalDirections;
 	} 
+
+    public void move(String coordinate){
+        //turn 
+        turn(coordinate.substring(0,1));
+        //move
+        walk(Integer.parseInt((coordinate.substring(1))));
+    }
 
     public void turn(String side){
         switch (side) {
@@ -29,4 +38,29 @@ public class Person {
                 break;
         }
     }
+
+    public void walk(int steps){
+        switch (facing()) {
+            case 0:
+                mapLocation.set(1, (mapLocation.get(1)+steps));  
+                break;
+
+            case 1:
+                mapLocation.set(0, (mapLocation.get(0)+steps));  
+                break;
+
+            case 2:
+                mapLocation.set(1, (mapLocation.get(1)-steps));
+                break;
+
+            case 3:
+                mapLocation.set(0, (mapLocation.get(0)-steps)); 
+                break;
+
+            default:
+                break;
+        }
+        
+    }
+    
 }
--- src/main/java/Day_01/Solution_1_Day_1.java
+++ src/main/java/Day_01/Solution_1_Day_1.java
@@ -3,18 +3,23 @@ package Day_01;
 import Shared.ReadFile;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.ArrayList;
 
 public class Solution_1_Day_1 {
 
 	int solution = 0;
 	
+	Person me = new Person();
+
 	public int getSolution() {
 		ReadFile rf = new ReadFile();
 		ArrayList<String> file = new ArrayList<String>();
-		String filepath = "./src/main/resources/Day_01_test_2016.txt";
-		/* String filepath = "./src/main/resources/Day_01_2016.txt"; */
+		/* String filepath = "./src/main/resources/Day_01_test_2016.txt"; */
+		String filepath = "./src/main/resources/Day_01_2016.txt";
 		file = rf.getInput(filepath);
+
+		
 
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
@@ -24,7 +29,15 @@ public class Solution_1_Day_1 {
 
 			/* p(coordinates); */
 
-			/* int answer = getAnswer(coordinates); */
+			if (coordinates instanceof List) {
+				List<String> coordinateList = (List<String>) coordinates;
+
+				for (String coordinate : coordinateList) {
+					me.move(coordinate);
+				}
+			}
+			
+			solution = getDistanceFromHQ();
 			
 
 		}
@@ -33,7 +46,11 @@ public class Solution_1_Day_1 {
 		return solution;
 	}
 	
-
+	public int getDistanceFromHQ(){
+		int x;
+		x = Math.abs(me.mapLocation.get(0) + me.mapLocation.get(1));
+		return x;
+	}
 	
 
 
