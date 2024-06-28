3062f3de664ce394a3bee0dcc50a48b5b62ee945
ourie <ourie@ourienb23.ahasw.cz>
2024-06-28 08:31:36 +0200

    Day_3 completed

--- src/main/java/Day_03/Solution_2_Day_3.java
+++ src/main/java/Day_03/Solution_2_Day_3.java
@@ -10,29 +10,36 @@ public class Solution_2_Day_3 {
     public int getSolution() {
         ReadFile rf = new ReadFile();
         ArrayList<String> file = new ArrayList<String>();
-        String filepath = "./src/main/resources/Day_03_test_2016.txt";
-        /* String filepath = "./src/main/resources/Day_03_2016.txt"; */
+        /* String filepath = "./src/main/resources/Day_03_test_2016.txt"; */
+        String filepath = "./src/main/resources/Day_03_2016.txt";
         file = rf.getInput(filepath);
 
         int possibleTriangles = 0;
         ArrayList<Integer> t = new ArrayList<Integer>();
 
         // Insert Solution Here:
+        
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < file.size(); j++) {
-                
                     String[] triangleLenghts = file.get(j).split(" ");
+                    t.add(numb(triangleLenghts[i]));  
                     
-                    t.add(numb(triangleLenghts[0]));
-                    
-                    if(j%3==0 && j!=0){
-                        p(t.get(0)+" "+ t.get(1)+" "+t.get(2));
-                        possibleTriangles += checkPossibleTriangles(t.get(0),t.get(1),t.get(2));
-                        t.clear();
-                    }
-                
             }
-        }
+        }   
+        
+            int i = 0;
+            while(i<=file.size()*3-3){
+                possibleTriangles += checkPossibleTriangles(t.get(i),t.get(i+1),t.get(i+2));
+                 p(t.get(i)+" "+ t.get(i+1)+" "+t.get(i+2));
+                p("i: "+i); 
+                p("psbT: "+possibleTriangles);
+                i+=3;
+            }
+            
+            
+            
+            
+        
         solution = possibleTriangles;
         return solution;
     }
