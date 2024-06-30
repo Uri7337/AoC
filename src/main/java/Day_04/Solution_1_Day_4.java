feecf8d9a0bb216be8bfe7fd394cf09abfcb866f
ourie <ourie@ourienb23.ahasw.cz>
2024-06-30 19:32:06 +0200

    progress on day 4 and java settings in visual

--- src/main/java/Day_04/Solution_1_Day_4.java
+++ src/main/java/Day_04/Solution_1_Day_4.java
@@ -3,6 +3,7 @@ package Day_04;
 import Shared.ReadFile;
 import Shared.AlphabetContainer;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Solution_1_Day_4 {
 
@@ -15,13 +16,35 @@ public class Solution_1_Day_4 {
         /* String filepath = "./src/main/resources/Day_04_2016.txt"; */
         file = rf.getInput(filepath);
 
-        AlphabetContainer alcont = new AlphabetContainer("Small");
+       // AlphabetContainer alcont = new AlphabetContainer("Small");
+
+       HashMap<String, Integer> commonLetters = new HashMap<String, Integer>();
+       String compareResult = "";
 
         // Insert Solution Here:
         for (int i = 0; i < file.size(); i++) {
             String[] lineParts = file.get(i).split("-");
             
-           
+            for (int j = 0; j < lineParts.length-1; j++) {
+                String[] linePartsPieces = lineParts[j].split("");
+                for (int k = 0; k < linePartsPieces.length; k++) {
+                    //p(linePartsPieces[k]);
+                    if(commonLetters.get(linePartsPieces[k]) != null){
+                        commonLetters.put(linePartsPieces[k], commonLetters.get(linePartsPieces[k])+1);
+                    }else{
+                        commonLetters.put(linePartsPieces[k], 1);
+                    }
+                }
+            }
+            //counting and sortin whole line of letters
+            for (String l : commonLetters.keySet()) {
+                //p(l);
+                compareResult +=l;
+            }
+            System.out.println(compareResult);
+            System.out.println(commonLetters);
+            commonLetters.clear();
+            compareResult = "";
         }
         
         solution = 0;
