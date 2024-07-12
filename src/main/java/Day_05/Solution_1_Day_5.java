7b234cb88da21389ea7507fb6ef29202702b9710
ourie <ourie@ourienb23.ahasw.cz>
2024-07-12 08:30:00 +0200

    Day_5 preparation

--- src/main/java/Day_05/Solution_1_Day_5.java
+++ src/main/java/Day_05/Solution_1_Day_5.java
@@ -1,89 +1,32 @@
 package Day_05;
 
 import Shared.ReadFile;
+
 import java.util.ArrayList;
 
 public class Solution_1_Day_5 {
 
-    int solution = 0;
-    ArrayList<String> file = new ArrayList<String>();
+	Object solution = "Nothing yet!";
 
-    public int getSolution() {
-        ReadFile rf = new ReadFile();
+	public Object getSolution() {
+		ReadFile rf = new ReadFile();
+		ArrayList<String> file = new ArrayList<String>();
+		String filepath = "./src/main/resources/Day_05_test_2016.txt";
+	/* 	String filepath = "./src/main/resources/Day_05_2016.txt"; */
+		file = rf.getInput(filepath);
 
-//        String filepath = "./src/main/resources/Day_5_test_2016.txt";
-        String filepath = "./src/main/resources/Day_5_2016.txt";
-        file = rf.getInput(filepath);
+		//Insert Solution Here:
+		for (int i = 0; i < file.size(); i++) {
+			String line = file.get(i);
+			
+		}
 
-        //Insert Solution Here:
-        for (int row = 0; row < file.size(); row++) {
+		return solution;
+	}
+   // prints content, System.out.println is way too long
+   public void p(Object content) {
+	System.out.println(content.toString());
+	}
 
-            System.out.println(file.get(row));
-            System.out.println(!isForbidden(row));
-            System.out.println(gotvowels(row));
-            System.out.println(gotdoubles(row));
-
-            if (!isForbidden(row)) {
-                if (gotvowels(row) && gotdoubles(row)) {
-                    solution++;
-                }
-            }
-
-        }
-
-        return solution;
-    }
-
-    public boolean gotvowels(int filerow) {
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        int containsvow = 0;
-        int vow = 0;
-        for (char v : vowels) {
-            for (int i = 0; i < file.get(filerow).length(); i++) {
-                containsvow = Character.compare(file.get(filerow).charAt(i),v);
-                if (containsvow == 0) {
-                    vow++;
-                    if (vow >= 3) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean gotdoubles(int filerow) {
-        char currletter = ' ';
-        char before = ' ';
-        char after = ' ';
-
-        for (int i = 0; i < file.get(filerow).length(); i++) {
-
-            currletter = file.get(filerow).charAt(i);
-
-            before = i == 0 ? ' ' : file.get(filerow).charAt(i - 1);
-            after = i == (file.get(filerow).length() - 1) ? ' ' : file.get(filerow).charAt(i + 1);
-
-            int comparebefore = Character.compare(currletter, before);
-            int compareafter = Character.compare(currletter, after);
-            if (comparebefore == 0 || compareafter == 0) {
-                return true;
-
-            }
-        }
-        return false;
-    }
-
-    public boolean isForbidden(int filerow) {
-        String[] forb = {"ab", "cd", "pq", "xy"};
-        boolean forbidden;
-        for (String i : forb) {
-            forbidden = file.get(filerow).contains(i);
-            if (forbidden) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+	// ----debug zone----
 }
--- src/main/java/Day_05/Solution_2_Day_5.java
+++ src/main/java/Day_05/Solution_2_Day_5.java
@@ -1,85 +1,32 @@
 package Day_05;
 
 import Shared.ReadFile;
+
 import java.util.ArrayList;
 
 public class Solution_2_Day_5 {
 
-    int solution = 0;
-    ArrayList<String> file = new ArrayList<String>();
+	Object solution = "Nothing yet!";
 
-    public int getSolution() {
-        ReadFile rf = new ReadFile();
+	public Object getSolution() {
+		ReadFile rf = new ReadFile();
+		ArrayList<String> file = new ArrayList<String>();
+		String filepath = "./src/main/resources/Day_05_test_2016.txt";
+	/* 	String filepath = "./src/main/resources/Day_05_2016.txt"; */
+		file = rf.getInput(filepath);
 
-//        String filepath = "./src/main/resources/Day_5_test_2016.txt";
-        String filepath = "./src/main/resources/Day_5_2016.txt";
-        
-        file = rf.getInput(filepath);
+		//Insert Solution Here:
+		for (int i = 0; i < file.size(); i++) {
+			String line = file.get(i);
 
-        //Insert Solution Here:
-        for (int row = 0; row < file.size(); row++) {
-			if (!pair(row)) System.out.println((row+1)+": nodpl");
-			else if (!repeats(row)) System.out.println((row+1)+": norepeat");
-			else System.out.println((row+1)+": ok");
-            
-            
-            if (pair(row) && repeats(row)) {
-                solution++;
+		}
 
-            }
+		return solution;
+	}
+   // prints content, System.out.println is way too long
+   public void p(Object content) {
+	System.out.println(content.toString());
+	}
 
-        }
-
-        return solution;
-    }
-
-    public boolean pair(int filerow) {
-		final String row = file.get(filerow);
-
-        for (int i = 0; i < row.length()-3; i++) {
-//            ArrayList<Integer> indexarr = new ArrayList<Integer>();
-            String word = row;
-           char gfch = word.charAt(i);
-           char gsch = word.charAt(i + 1);
-
-            String guess = gfch + "" + gsch;
-
-            int index = word.indexOf(guess,i+2);
-			
-//            indexarr.add(index);
-            
-            if (index >= 0) {
-                return true;
-            }
-            
-//            int firstloc = indexarr.get(0)+1;
-//            int secondloc = indexarr.get(1);
-            
-//            if(indexarr.size()>=3){
-//                System.out.println(indexarr);
-//                return true;
-//            }
-        }
-        return false;
-    }
-
-    public boolean repeats(int filerow) {
-        char cl = ' '; //currletter
-        char csl = ' '; //currsameletter
-
-        for (int i = 0; i < (file.get(filerow).length() - 2); i++) {
-            String input = file.get(filerow);
-            cl = input.charAt(i);
-
-            csl = input.charAt(i + 2);
-
-            if (cl == csl) {
-//                System.out.println(cl+""+input.charAt(i + 1)+""+csl);
-                return true;
-            }
-
-        }
-        return false;
-    }
-
+	// ----debug zone----
 }
