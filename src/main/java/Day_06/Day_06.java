7214affef6e7d6bfde22adcde354bc83539b3859
ourie <ourie@ourienb23.ahasw.cz>
2024-07-12 11:22:15 +0200

    Day_06 preparation

--- src/main/java/Day_06/Day_06.java
+++ src/main/java/Day_06/Day_06.java
@@ -1,13 +1,14 @@
 package Day_06;
 
+import Shared.EasyPrint;
 public class Day_06 {
-	public static void main (String[] args) {
-		Solution_1_Day_06 s1 = new Solution_1_Day_06();
-		Solution_2_Day_06 s2 = new Solution_2_Day_06();
 
-		//Change for wanted solution
-//		boolean Solution1 = true;
-		boolean Solution1 = false;
-		System.out.println(Solution1 ? "Day_06_Solution1: " + s1.getSolution() : "Day_06_Solution2: " + s2.getSolution());
-	}
+    public static void main(String[] args) {
+        EasyPrint ep = new EasyPrint();
+        Solution_1_Day_06 s1 = new Solution_1_Day_06();
+        Solution_2_Day_06 s2 = new Solution_2_Day_06();
+
+        ep.p("Day_06_Solution1: " + s1.getSolution().toString());
+        ep.p("Day_06_Solution2: " + s2.getSolution().toString());
+    }
 }
