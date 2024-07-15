4037dead891276e90b7102c31832d64f44e27e65
ourie <ourie@ourienb23.ahasw.cz>
2024-07-15 09:36:30 +0200

    feat: Day_06 completed feat: added one line print to EasyPrint

--- src/main/java/Day_06/Solution_1_Day_06.java
+++ src/main/java/Day_06/Solution_1_Day_06.java
@@ -4,6 +4,11 @@ import Shared.ReadFile;
 import Shared.EasyPrint;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 
 public class Solution_1_Day_06 {
@@ -18,13 +23,34 @@ public class Solution_1_Day_06 {
 		String filepath = "./src/main/resources/Day_06_2016.txt";
 		file = rf.getInput(filepath);
 
-		//Insert Solution Here:
-		for (int i = 0; i < file.size(); i++) {
-			String line = file.get(i);
-			
-		}
+		Map<Character, Integer> commonLetters = new HashMap<Character, Integer>();
+		String sol = "";
 
-		return solution;
+		//Insert Solution Here:
+		for (int i = 0; i < file.get(0).length(); i++) {
+			for (int j = 0; j < file.size(); j++) {
+				String line = file.get(j);
+				/* ep.p(line.charAt(i)); */
+				commonLetters.put(line.charAt(i), commonLetters.getOrDefault(line.charAt(i),0)+1);
+				
+			}
+			
+			List<Entry<Character, Integer>> entrySet = new ArrayList<>(commonLetters.entrySet());
+			commonLetters.clear();
+
+			entrySet.sort(
+                Comparator
+                    .<Entry<Character, Integer>>comparingInt(value -> value.getValue())
+                    .reversed()
+                    .thenComparing(Entry::getKey)
+            );
+
+
+			
+			sol = sol + entrySet.get(0).getKey();
+
+		}
+		return solution = sol;
 	}
 
 	// ----debug zone----
--- src/main/java/Shared/EasyPrint.java
+++ src/main/java/Shared/EasyPrint.java
@@ -4,4 +4,7 @@ public class EasyPrint {
     public void p(Object content) {
         System.out.println(content.toString());
     }
+    public void np(Object content) {
+        System.out.print(content.toString());
+    }
 }
