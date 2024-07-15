1c473e8fcbd246cc11149e6a4ffb332306ec8d71
ourie <ourie@ourienb23.ahasw.cz>
2024-07-15 13:03:39 +0200

    Day_04 Completed

--- src/main/java/Day_04/Solution_2_Day_04.java
+++ src/main/java/Day_04/Solution_2_Day_04.java
@@ -3,6 +3,12 @@ package Day_04;
 import Shared.EasyPrint;
 import Shared.ReadFile;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.util.Map.Entry;
 
 public class Solution_2_Day_04 {
     EasyPrint ep = new EasyPrint();
@@ -18,14 +24,49 @@ public class Solution_2_Day_04 {
 
         // Insert Solution Here:
         for (int i = 0; i < file.size(); i++) {
-            //  file.get(i).split(" ");
-
+            String[] lineParts = file.get(i).split("-");
+            
+            int roomID =  Integer.parseInt(lineParts[lineParts.length-1].substring(0, 3));
+            StringBuffer sb = new StringBuffer();
+            for (int j = 0; j < lineParts.length-1; j++) {
+                sb.append(encrypt(lineParts[j],roomID)+" ");
                 
+            }
+            sb.append(roomID);
+
+            if(sb.toString().startsWith("north")){
+                ep.p(sb);
+                solution = sb.toString();
+            }
         }
-        
+
 
         return solution;
     }
+
+    /* from https://www.geeksforgeeks.org/caesar-cipher-in-cryptography/ */
+    public static StringBuffer encrypt(String text, int s)
+    {
+        StringBuffer result= new StringBuffer();
+
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 65) % 26 + 65);
+                result.append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+   
 
     // ----debug zone----
 
