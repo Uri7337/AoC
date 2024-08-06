bd49ba91971938490d6b7e9b8f33ba3bf1e30ccf
ourie <ourie@ourienb23.ahasw.cz>
2024-08-06 10:37:25 +0200

    Day_02 completed

--- src/main/java/Day_02/Solution_1_Day_02.java
+++ src/main/java/Day_02/Solution_1_Day_02.java
@@ -10,7 +10,7 @@ public class Solution_1_Day_02 {
 	EasyPrint ep = new EasyPrint();
 
 	Object solution = "Nothing yet!";
-
+	
 	public Object getSolution() {
 		ReadFile rf = new ReadFile();
 		ArrayList<String> file = new ArrayList<String>();
@@ -18,12 +18,63 @@ public class Solution_1_Day_02 {
 		String filepath = "./src/main/resources/Day_02_2023.txt";
 		file = rf.getInput(filepath);
 
+		int possibleGamesIDCount = 0;
+		
+
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
+
+			String[] lineParts = line.split(":");
+
+			int gameID = Integer.parseInt(lineParts[0].split(" ")[1]);
+			
+
+			String[] sets = lineParts[1].split(";");
+			int possibleSets = 0;
+
+			for (String set : sets) {
+				//cubes
+				int rC = 12; //red
+				int gC = 13; //green
+				int bC = 14; //blue
+
+				
+				String [] setParts = set.split(",");
+				for (String singleCube : setParts) {
+					int cubeCount = Integer.parseInt(singleCube.split(" ")[1]);
+					String cubeColor = singleCube.split(" ")[2];
+
+					switch (cubeColor) {
+						case "red":
+							rC-=cubeCount;
+							break;
+						case "green":
+							gC-=cubeCount;
+							break;
+						case "blue":
+							bC-=cubeCount;
+							break;
+					
+						default:
+						ep.p("Game "+gameID+": "+"color did not recognized");
+					}
+				}
+
+				if(0 <= rC && 0 <= gC && 0 <= bC){
+					possibleSets++;
+				}
+
+				if(possibleSets==sets.length){
+					
+					possibleGamesIDCount+=gameID;
+					
+				}
+			}
 			
 		}
 
+		solution = possibleGamesIDCount;
 		return solution;
 	}
 
--- src/main/java/Day_02/Solution_2_Day_02.java
+++ src/main/java/Day_02/Solution_2_Day_02.java
@@ -10,7 +10,7 @@ public class Solution_2_Day_02 {
 	EasyPrint ep = new EasyPrint();
 
 	Object solution = "Nothing yet!";
-
+	
 	public Object getSolution() {
 		ReadFile rf = new ReadFile();
 		ArrayList<String> file = new ArrayList<String>();
@@ -18,12 +18,69 @@ public class Solution_2_Day_02 {
 		String filepath = "./src/main/resources/Day_02_2023.txt";
 		file = rf.getInput(filepath);
 
+		int possibleGamesIDCount = 0;
+		
+
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
+
+			String[] lineParts = line.split(":");
+
+			int gameID = Integer.parseInt(lineParts[0].split(" ")[1]);
+			
+
+			String[] sets = lineParts[1].split(";");
+			int setsCount = 0;
+
+			//cubes
+			int rC = 0; //red
+			int gC = 0; //green
+			int bC = 0; //blue
+
+			for (String set : sets) {
+				
+
+				
+				String [] setParts = set.split(",");
+				for (String singleCube : setParts) {
+					int cubeCount = Integer.parseInt(singleCube.split(" ")[1]);
+					String cubeColor = singleCube.split(" ")[2];
+
+					switch (cubeColor) {
+						case "red":
+							if(cubeCount>rC){
+								rC=cubeCount;
+							}
+							
+							break;
+						case "green":
+							if(cubeCount>gC){
+								gC=cubeCount;
+							}
+							break;
+						case "blue":
+							if(cubeCount>bC){
+								bC=cubeCount;
+							}
+							break;
+					
+						default:
+						ep.p("Game "+gameID+": "+"color did not recognized");
+					}
+				}
+
+				setsCount++;
+				if(setsCount==sets.length){
+					
+					possibleGamesIDCount+= (rC*gC*bC);
+					
+				}
+			}
 			
 		}
-
+		
+		solution = possibleGamesIDCount;
 		return solution;
 	}
 
