307990e8d5bd5f5b55a44195e9fe603af995b07b
ourie <ourie@ourienb23.ahasw.cz>
2024-08-06 08:18:39 +0200

    Day_02 updates

---
+++ src/main/java/Day_02/Solution_2_Day_02.java
@@ -0,0 +1,32 @@
+package Day_02;
+
+import shared.ReadFile;
+import shared.EasyPrint;
+
+import java.util.ArrayList;
+
+
+public class Solution_2_Day_02 {
+	EasyPrint ep = new EasyPrint();
+
+	Object solution = "Nothing yet!";
+
+	public Object getSolution() {
+		ReadFile rf = new ReadFile();
+		ArrayList<String> file = new ArrayList<String>();
+		/* String filepath = "./src/main/resources/Day_02_test_2023.txt"; */
+		String filepath = "./src/main/resources/Day_02_2023.txt";
+		file = rf.getInput(filepath);
+
+		//Insert Solution Here:
+		for (int i = 0; i < file.size(); i++) {
+			String line = file.get(i);
+			
+		}
+
+		return solution;
+	}
+
+	// ----debug zone----
+	
+}
