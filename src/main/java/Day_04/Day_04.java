a41c0466748ac1f61ce7d6cc2da9b5b8205bf0f0
ourie <ourie@ourienb23.ahasw.cz>
2024-08-15 10:31:32 +0200

    Day_04 prep

--- src/main/java/Day_04/Day_04.java
+++ src/main/java/Day_04/Day_04.java
@@ -1,12 +1,25 @@
-//
 package Day_04;
-//
+
+import shared.EasyPrint;
 public class Day_04 {
-    public static void main (String[] args) {
-        Solution_1_Day_4 s1 = new Solution_1_Day_4();
-        Solution_2_Day_4 s2 = new Solution_2_Day_4();
-//
-	 System.out.println("Day_4_Solution1:" + s1.getSolution());
-	 System.out.println("Day_4_Solution2:" + s2.getSolution());
+
+    public static void main(String[] args) {
+        EasyPrint ep = new EasyPrint();
+        Solution_1_Day_04 s1 = new Solution_1_Day_04();
+        Solution_2_Day_04 s2 = new Solution_2_Day_04();
+
+        String filepath_test = "./src/main/resources/Day_04_test_2023.txt";
+		String filepath = "./src/main/resources/Day_04_2023.txt";
+        
+        ep.p("\n");
+        ep.p("    Day_04");
+        ep.p("===Solutions===");
+        ep.p("Solution 1");
+        ep.p("Tests: " + s1.getSolution(filepath_test).toString());
+        ep.p("Answer: " + s1.getSolution(filepath).toString());
+        ep.p("");
+        ep.p("Solution 2");
+        ep.p("Tests: " + s2.getSolution(filepath_test).toString());
+        ep.p("Answer: " + s2.getSolution(filepath).toString());
     }
 }
