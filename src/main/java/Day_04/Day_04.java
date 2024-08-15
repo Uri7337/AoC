31026fb3f00211273f3dac112582b7d76346657b
ourie <ourie@ourienb23.ahasw.cz>
2024-08-15 11:47:44 +0200

    Day_04 Sol_1 Completed

--- src/main/java/Day_04/Day_04.java
+++ src/main/java/Day_04/Day_04.java
@@ -11,15 +11,26 @@ public class Day_04 {
         String filepath_test = "./src/main/resources/Day_04_test_2023.txt";
 		String filepath = "./src/main/resources/Day_04_2023.txt";
         
+        ep.p("==Start==");
+        ep.p("==Test prints==");
+        String sol1_test = s1.getSolution(filepath_test).toString();
+        ep.p("\n==Answer prints==");
+        String sol1 = s1.getSolution(filepath).toString();
+        ep.p("\n==Test prints==");
+        String sol2_test = s2.getSolution(filepath_test).toString();
+        ep.p("\n==Answer prints==");
+        String sol2 = s2.getSolution(filepath).toString();
+        ep.p("\n==End==");
+        
         ep.p("\n");
         ep.p("    Day_04");
         ep.p("===Solutions===");
         ep.p("Solution *");
-        ep.p("Tests: " + s1.getSolution(filepath_test).toString());
-        ep.p("Answer: " + s1.getSolution(filepath).toString());
+        ep.p("Tests: " + sol1_test);
+        ep.p("Answer: " + sol1);
         ep.p("");
         ep.p("Solution **");
-        ep.p("Tests: " + s2.getSolution(filepath_test).toString());
-        ep.p("Answer: " + s2.getSolution(filepath).toString());
+        ep.p("Tests: " + sol2_test);
+        ep.p("Answer: " + sol2);
     }
 }
--- src/main/java/Day_04/Solution_1_Day_04.java
+++ src/main/java/Day_04/Solution_1_Day_04.java
@@ -11,9 +11,11 @@ public class Solution_1_Day_04 {
 
 	Object solution = "Nothing yet!";
 	
+
 	ArrayList<String> file;
 	public Object getSolution(String filepath) {
-		
+		int result = 0;
+        
 		ReadFile rf = new ReadFile();
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
@@ -21,10 +23,39 @@ public class Solution_1_Day_04 {
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
+            String[] lineParts = line.split(":");
 
+            int cardID = Integer.parseInt(lineParts[0].split("  *")[1]);
+            String[] winningNumbers = lineParts[1].split("\\|")[0].split("  *");
+            String[] myNumbers = lineParts[1].split("\\|")[1].split("  *");
+
+            ArrayList<Integer> wN = convertNumbers(winningNumbers);
+            ArrayList<Integer> mN = convertNumbers(myNumbers);
+            mN.retainAll(wN);
+            
+            if(mN.size()>0){
+                
+                int res =1;
+                for (int j = 1; j < mN.size(); j++) {
+                    res*=2;
+                }
+                ep.p(mN + " size: "+mN.size() + " result: "+ res);
+                result += res;
+                ep.p(result);
+            }
+            
         }
-		return solution;
+		return solution = result;
 	}
+
+    ArrayList<Integer> convertNumbers(String[] numbers){
+        ArrayList<Integer> aL = new ArrayList<>();
+        for (int i = 1; i < numbers.length; i++) {
+            aL.add(Integer.parseInt(numbers[i]));
+        }
+
+        return aL;
+    }
 
 	// ----debug zone----
 	
