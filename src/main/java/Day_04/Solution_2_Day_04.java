f00e54486eabd0e4a24b30f68f5a063d11041849
ourie <ourie@ourienb23.ahasw.cz>
2024-08-15 19:57:18 +0200

    Day_04 completed

--- src/main/java/Day_04/Solution_2_Day_04.java
+++ src/main/java/Day_04/Solution_2_Day_04.java
@@ -4,6 +4,7 @@ import shared.ReadFile;
 import shared.EasyPrint;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Solution_2_Day_04 {
@@ -11,20 +12,73 @@ public class Solution_2_Day_04 {
 
 	Object solution = "Nothing yet!";
 	
+
 	ArrayList<String> file;
 	public Object getSolution(String filepath) {
-		
+		int result = 0;
+        
 		ReadFile rf = new ReadFile();
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
 			
+        ArrayList<Card> cards = new ArrayList<Card>();
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
+            String[] lineParts = line.split(":");
 
+            int cardID = Integer.parseInt(lineParts[0].split("  *")[1]);
+            String[] winningNumbers = lineParts[1].split("\\|")[0].split("  *");
+            String[] myNumbers = lineParts[1].split("\\|")[1].split("  *");
+
+            ArrayList<Integer> wN = convertNumbers(winningNumbers);
+            ArrayList<Integer> mN = convertNumbers(myNumbers);
+            mN.retainAll(wN);
+            int copies = mN.size();
+            Card c = new Card(cardID, copies);
+            cards.add(c);
         }
-		return solution;
+
+        for (int i = 0; i < cards.size(); i++) {
+            for (int k = 0; k < cards.get(i).cardCopies; k++) {
+                /* ep.p(cards.get(i).cardID); */
+                for (int j = cards.get(i).cardID; j < cards.get(i).cardID+cards.get(i).copies; j++) {
+                    cards.get(j).cardCopies++;
+                    /* ep.p("card: "+cards.get(j).cardID + " card instances: " +cards.get(j).cardCopies); */
+                }
+                /* ep.p(""); */
+            }
+           
+          }
+         /*  ep.p("") */;
+          for (Card c : cards) {
+            /* ep.p("card: "+c.cardID + " card instances: " +c.cardCopies); */
+            result += c.cardCopies;
+          }
+		return solution = result;
 	}
+
+    ArrayList<Integer> convertNumbers(String[] numbers){
+        ArrayList<Integer> aL = new ArrayList<>();
+        for (int i = 1; i < numbers.length; i++) {
+            aL.add(Integer.parseInt(numbers[i]));
+        }
+
+        return aL;
+    }
+
+    class Card{
+        int cardID;
+        int cardCopies;
+        int copies;
+        
+        Card(int cardID,int copies){
+            this.cardID = cardID;
+            this.copies = copies;
+            this.cardCopies = 1;
+        }
+
+    }
 
 	// ----debug zone----
 	
