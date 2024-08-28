20705d15bb351dba98cc510c1c0c62dcdd38f693
ourie <ourie@ourienb23.ahasw.cz>
2024-08-28 13:22:45 +0200

    Day_05 progress

--- src/main/java/Day_05/Day_05.java
+++ src/main/java/Day_05/Day_05.java
@@ -14,16 +14,29 @@ public class Day_05 {
         String filepath_test = "./src/main/resources/Day_05_test_2023.txt";
 		String filepath = "./src/main/resources/Day_05_2023.txt";
         
+        ep.p("==Start==");
+        ep.p("Solution *");
+        ep.p("==Test prints==");
+        String sol1_test = s1.getSolution(filepath_test).toString();
+        ep.p("\n==Answer prints==");
+        String sol1 = s1.getSolution(filepath).toString();
+        ep.p("Solution **");
+        ep.p("\n==Test prints==");
+        String sol2_test = s2.getSolution(filepath_test).toString();
+        ep.p("\n==Answer prints==");
+        String sol2 = s2.getSolution(filepath).toString();
+        ep.p("\n==End==");
+        
         ep.p("\n");
         ep.p("    Day_05");
         ep.p("===Solutions===");
         ep.p("Solution *");
-        ep.p("Tests: " + s1.getSolution(filepath_test).toString());
-        ep.p("Answer: " + s1.getSolution(filepath).toString());
+        ep.p("Tests: " + sol1_test);
+        ep.p("Answer: " + sol1);
         ep.p("");
         ep.p("Solution **");
-        ep.p("Tests: " + s2.getSolution(filepath_test).toString());
-        ep.p("Answer: " + s2.getSolution(filepath).toString());
+        ep.p("Tests: " + sol2_test);
+        ep.p("Answer: " + sol2);
     }
   
 }
--- src/main/java/Day_05/Solution_1_Day_05.java
+++ src/main/java/Day_05/Solution_1_Day_05.java
@@ -18,9 +18,41 @@ public class Solution_1_Day_05 {
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
 			
+		ArrayList<ArrayList<Long>> seedsList = new ArrayList<ArrayList<Long>>();
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
+
+			if (line.contains("seeds:")){
+				//seeds
+				ArrayList<Long> seedList;
+				String[] seeds = line.split(" ");
+				for (int j = 1; j < seeds.length; j++) {
+					seedList = new ArrayList<Long>();
+					seedList.add(Long.parseLong(seeds[j]));
+					seedsList.add(seedList);
+				}
+
+				for (int k = 0; k < seedsList.size(); k++) { 
+					for (int j = 0; j < seedsList.get(k).size(); j++) { 
+						System.out.print(seedsList.get(k).get(j) + " "); 
+					} 
+					System.out.println(); 
+				} 
+
+			}else{
+				if (line.contains("-to-")) {
+					//seed-to-soil map:
+					
+
+				} else if (line.matches("[\\d]* [\\d*]* [\\d]*")){
+					//numbers
+					long n1 = Long.parseLong(line.split(" ")[0]);
+					long n2 = Long.parseLong(line.split(" ")[1]);
+					long n3 = Long.parseLong(line.split(" ")[2]);
+					ep.p(n1 +"-"+ n2 +"-"+ n3);
+				}
+			}
 
         }
 		return solution;
