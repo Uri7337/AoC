b509bb4d3abcf82f250e12c2085ef6b36eca301f
ourie <ourie@ourienb23.ahasw.cz>
2024-09-03 10:26:05 +0200

    Day_05 really decent progress, im happy about it! :)

--- src/main/java/Day_05/Day_05.java
+++ src/main/java/Day_05/Day_05.java
@@ -1,6 +1,7 @@
 package Day_05;
 
 import shared.EasyPrint;
+
 public class Day_05 {
 
     public static void main(String[] args) {
@@ -9,31 +10,86 @@ public class Day_05 {
         Solution_2_Day_05 s2 = new Solution_2_Day_05();
 
         String filepath_test = "./src/main/resources/Day_05_test_2023.txt";
-		String filepath = "./src/main/resources/Day_05_2023.txt";
-        
-        ep.p("==Start==");
-        ep.p("Solution *");
-        ep.p("==Test prints==");
+        String filepath = "./src/main/resources/Day_05_2023.txt";
+        // my way
+        /*
+         * ep.p("========Start========");
+         * ep.p("==Start Solution*==");
+         * ep.p("");
+         * ep.p("==Test prints==");
+         * String sol1_test = s1.getSolution(filepath_test).toString();
+         * ep.p("\n==Answer prints==");
+         * String sol1 = s1.getSolution(filepath).toString();
+         * ep.p("\n==End Solution*==");
+         * ep.p("");
+         * ep.p("==Start Solution**==");
+         * ep.p("\n==Test prints==");
+         * String sol2_test = s2.getSolution(filepath_test).toString();
+         * ep.p("\n==Answer prints==");
+         * String sol2 = s2.getSolution(filepath).toString();
+         * ep.p("\n==End Solution**==");
+         * ep.p("\n========End========");
+         * 
+         * ep.p("\n");
+         * ep.p("    Day_05");
+         * ep.p("===Solutions===");
+         * ep.p("Solution *");
+         * ep.p("Tests: " + sol1_test);
+         * ep.p("Answer: " + sol1);
+         * ep.p("");
+         * ep.p("Solution **");
+         * ep.p("Tests: " + sol2_test);
+         * ep.p("Answer: " + sol2);
+         */
+
+        // chatgpt
+
+        ep.p("=====================================");
+        ep.p("========       Start        =========");
+        ep.p("=====================================");
+        ep.p("");
+
+        ep.p("************* Start Solution * *************");
+        ep.p("");
+        ep.p("== Test prints ==");
         String sol1_test = s1.getSolution(filepath_test).toString();
-        ep.p("\n==Answer prints==");
+        ep.p("");
+        ep.p("== Answer prints ==");
         String sol1 = s1.getSolution(filepath).toString();
-        ep.p("Solution **");
-        ep.p("\n==Test prints==");
+        ep.p("");
+        ep.p("************* End Solution * ***************");
+        ep.p("");
+
+        ep.p("************* Start Solution ** ************");
+        ep.p("");
+        ep.p("== Test prints ==");
         String sol2_test = s2.getSolution(filepath_test).toString();
-        ep.p("\n==Answer prints==");
+        ep.p("");
+        ep.p("== Answer prints ==");
         String sol2 = s2.getSolution(filepath).toString();
-        ep.p("\n==End==");
-        
-        ep.p("\n");
-        ep.p("    Day_05");
-        ep.p("===Solutions===");
+        ep.p("");
+        ep.p("************* End Solution ** **************");
+        ep.p("");
+
+        ep.p("=====================================");
+        ep.p("=========        End         ========");
+        ep.p("=====================================");
+        ep.p("");
+
+        ep.p("=====================================");
+        ep.p("                 Day_05               ");
+        ep.p("=====================================");
+        ep.p("************** Solutions **************");
         ep.p("Solution *");
-        ep.p("Tests: " + sol1_test);
-        ep.p("Answer: " + sol1);
+        ep.p("    Tests: " + sol1_test);
+        ep.p("    Answer: " + sol1);
         ep.p("");
         ep.p("Solution **");
-        ep.p("Tests: " + sol2_test);
-        ep.p("Answer: " + sol2);
+        ep.p("    Tests: " + sol2_test);
+        ep.p("    Answer: " + sol2);
+        ep.p("");
+        ep.p("=====================================");
+
     }
-  
+
 }
--- src/main/java/Day_05/Solution_1_Day_05.java
+++ src/main/java/Day_05/Solution_1_Day_05.java
@@ -4,6 +4,7 @@ import shared.ReadFile;
 import shared.EasyPrint;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Solution_1_Day_05 {
@@ -18,56 +19,129 @@ public class Solution_1_Day_05 {
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
 			
-		ArrayList<ArrayList<Long>> seedsList = new ArrayList<ArrayList<Long>>();
+		ArrayList<Seed> seedList = new ArrayList<Seed>();
+		ArrayList<Category> categoryList = new ArrayList<Category>();
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
 
 			if (line.contains("seeds:")){
 				//seeds
-				ArrayList<Long> seedList;
+				
 				String[] seeds = line.split(" ");
 				for (int j = 1; j < seeds.length; j++) {
-					seedList = new ArrayList<Long>();
-					seedList.add(Long.parseLong(seeds[j]));
-					seedsList.add(seedList);
+					Seed s = new Seed("seed", Long.parseLong(seeds[j]));
+					seedList.add(s);
 				}
-
-				/* for (int k = 0; k < seedsList.size(); k++) { 
-					for (int j = 0; j < seedsList.get(k).size(); j++) { 
-						System.out.print(seedsList.get(k).get(j) + " "); 
-					} 
-					System.out.println(); 
-				} */ 
 
 			}else{
 				if (line.contains("-to-")) {
 					//seed-to-soil map:
 					String from = line.split("-to-")[0];
 					String to = line.split("-to-")[1].split(" ")[0];
-					ep.p(from +"->" + to);
+					/* ep.p(from +"->" + to); */
+
+					Category c = new Category(from, to);
+					categoryList.add(c);
 
 
 				} else if (line.matches("[\\d]* [\\d*]* [\\d]*")){
-					//numbers
+					//numbers 
+					//add then to the last category in the list (their category will always be the last one in the list)
+					
 					long n1 = Long.parseLong(line.split(" ")[0]);
 					long n2 = Long.parseLong(line.split(" ")[1]);
 					long n3 = Long.parseLong(line.split(" ")[2]);
 					/* ep.p(n1 +"-"+ n2 +"-"+ n3); */
+					Range r = new Range(n2, n1, n3);
+					categoryList.get(categoryList.size()-1).ranges.add(r);
 				}
 			}
 
         }
+		/* 
+		now for test print
+		seedList
+		categoryList 
+		ranges in category
+
+		seedList.forEach(s -> s.readRecord());
+		ep.p("");
+		categoryList.forEach(cl -> {cl.printCategory(); ep.p("");});
+		 */
+		
+		
+
+		// for every seed go true every category and if in range change number else dont
+
 		return solution;
 	}
 
-	class seed {
-		//changing state based on where the seed is currently located e.g. if seed-> soil state:soil 
+	class Seed {
+		//changing state based on where the seed is currently located e.g. if seed -> soil state:soil 
 		String state;
-		int number;
-		ArrayList<Long> seedHistory = new ArrayList<Long>();
+		long number;
+		HashMap<String, Long> seedHistory;
+
+		public Seed(String state, long number) {
+			this.seedHistory = new HashMap<String, Long>();
+			this.state = state;
+			this.number = number;
+			this.seedHistory.put(state,number);
+		}
+
+		void makeRecord(String state, long number){
+			this.state = state;
+			this.number = number;
+			this.seedHistory.put(state,number);
+		}
+
+		void readRecord(){
+			for (String i : seedHistory.keySet()) {
+				ep.p("state: " + i + "; number: " + seedHistory.get(i));
+			  }
+		}
 
 	} 
+
+	class Category {
+		String from;
+		String name;
+		ArrayList<Range> ranges;
+
+		public Category(String from, String name) {
+			this.from = from;
+			this.name = name;
+			this.ranges = new ArrayList<Range>();
+		}
+
+		void printCategory(){
+				ep.p(from+"-to-"+name +" map:"); 
+				readRanges();
+		}
+
+		void readRanges(){
+			for (Range r : ranges) {
+				r.printRange();
+			  }
+		}
+	}
+
+	class Range {
+		long sourceRangeStart;
+		long destinationRange;
+		long rangeLength;
+
+		public Range(long sourceRangeStart, long destinationRange, long rangeLength) {
+			this.sourceRangeStart = sourceRangeStart;
+			this.destinationRange = destinationRange;
+			this.rangeLength = rangeLength;
+		}
+
+		void printRange(){
+			ep.p(destinationRange +" "+ sourceRangeStart +" "+rangeLength);
+		}
+	}
 
 	// ----debug zone----
 	
