3e6d09a875f5c6f6ed78603478ad2648a3ed25f3
Uri7337 <ondrasekuriasek@gmail.com>
2024-10-28 15:19:33 +0100

    Day_07 prep

--- src/main/java/Day_07/Day_07.java
+++ src/main/java/Day_07/Day_07.java
@@ -1,12 +1,64 @@
-
 package Day_07;
 
-public class Day_07 {
-    public static void main (String[] args) {
-        Solution_1_Day_7 s1 = new Solution_1_Day_7();
-//        Solution_2_Day_7 s2 = new Solution_2_Day_7();
+import shared.EasyPrint;
 
-	 System.out.println("Day_7_Solution1:" + s1.getSolution());
-//	 System.out.println("Day_7_Solution2:" + s2.getSolution());
+public class Day_07 {
+
+    public static void main(String[] args) {
+        EasyPrint ep = new EasyPrint();
+        Solution_1_Day_07 s1 = new Solution_1_Day_07();
+        Solution_2_Day_07 s2 = new Solution_2_Day_07();
+
+        String filepath_test = "./src/main/resources/Day_07_test_2023.txt";
+        String filepath = "./src/main/resources/Day_07_2023.txt";
+
+
+        ep.p("=====================================");
+        ep.p("========       Start        =========");
+        ep.p("=====================================");
+        ep.p("");
+
+        ep.p("************* Start Solution * *************");
+        ep.p("");
+        ep.p("== Test prints ==");
+        String sol1_test = s1.getSolution(filepath_test).toString();
+        ep.p("");
+        ep.p("== Answer prints ==");
+        String sol1 = s1.getSolution(filepath).toString();
+        ep.p("");
+        ep.p("************* End Solution * ***************");
+        ep.p("");
+
+        ep.p("************* Start Solution ** ************");
+        ep.p("");
+        ep.p("== Test prints ==");
+        String sol2_test = s2.getSolution(filepath_test).toString();
+        ep.p("");
+        ep.p("== Answer prints ==");
+        String sol2 = s2.getSolution(filepath).toString();
+        ep.p("");
+        ep.p("************* End Solution ** **************");
+        ep.p("");
+
+        ep.p("=====================================");
+        ep.p("=========        End         ========");
+        ep.p("=====================================");
+        ep.p("");
+
+        ep.p("=====================================");
+        ep.p("                 Day_07               ");
+        ep.p("=====================================");
+        ep.p("************** Solutions **************");
+        ep.p("Solution *");
+        ep.p("    Tests: " + sol1_test);
+        ep.p("    Answer: " + sol1);
+        ep.p("");
+        ep.p("Solution **");
+        ep.p("    Tests: " + sol2_test);
+       ep.p("    Answer: " + sol2);
+        ep.p("");
+        ep.p("=====================================");
+
     }
+
 }
---
+++ src/main/java/Day_07/Solution_1_Day_07.java
@@ -0,0 +1,37 @@
+package Day_07;
+
+import shared.ReadFile;
+import shared.EasyPrint;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Solution_1_Day_07 {
+	EasyPrint ep = new EasyPrint();
+
+	Object solution = "Nothing yet!";
+	
+	ArrayList<String> file;
+	public Object getSolution(String filepath) {
+		
+		ReadFile rf = new ReadFile();
+		file = new ArrayList<String>();
+		file = rf.getInput(filepath);
+			
+		
+		//Insert Solution Here:
+		for (int i = 0; i < file.size(); i++) {
+			String line = file.get(i);
+
+			
+
+		return solution;
+	}
+
+	
+
+	
+	// ----debug zone----
+	
+}
