bbef8225564ad41a97c1068b691095ece98c428f
Uri7337 <ondrasekuriasek@gmail.com>
2024-10-28 10:20:46 +0100

    final stage im gonna watch tutorial then move on another, bcs it s still problematic and i can t find bug in my code

--- src/main/java/Day_05/Day_05.java
+++ src/main/java/Day_05/Day_05.java
@@ -63,7 +63,7 @@ public class Day_05 {
         ep.p("************* Start Solution ** ************");
         ep.p("");
         ep.p("== Test prints ==");
-       // String sol2_test = s2.getSolution(filepath_test).toString();
+        String sol2_test = s2.getSolution(filepath_test).toString();
         ep.p("");
         ep.p("== Answer prints ==");
         String sol2 = s2.getSolution(filepath).toString();
@@ -85,7 +85,7 @@ public class Day_05 {
         ep.p("    Answer: " + sol1);
         ep.p("");
         ep.p("Solution **");
-       // ep.p("    Tests: " + sol2_test);
+        ep.p("    Tests: " + sol2_test);
        ep.p("    Answer: " + sol2);
         ep.p("");
         ep.p("=====================================");
--- src/main/java/Day_05/Solution_2_Day_05.java
+++ src/main/java/Day_05/Solution_2_Day_05.java
@@ -5,7 +5,6 @@ import shared.EasyPrint;
 
 import java.util.ArrayList;
 
-
 public class Solution_2_Day_05 {
 	EasyPrint ep = new EasyPrint();
 
@@ -15,36 +14,35 @@ public class Solution_2_Day_05 {
 	ArrayList<Seed> seedList = new ArrayList<Seed>();
 
 	public Object getSolution(String filepath) {
-		
+
 		ReadFile rf = new ReadFile();
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
-			
-		
+
 		ArrayList<Category> categoryList = new ArrayList<Category>();
-		//Insert Solution Here:
+		// Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
 
-			if (line.contains("seeds:")){
-				//seeds
-				
+			if (line.contains("seeds:")) {
+				// seeds
+
 				String[] seeds = line.split(" ");
 				boolean first = true;
 				for (int j = 1; j < seeds.length; j++) {
-					if (first){
+					if (first) {
 						Seed s = new Seed("seed", Long.parseLong(seeds[j]));
 						seedList.add(s);
-						first=false;
-					}else{
-						seedList.get(seedList.size()-1).rangeSize = Long.parseLong(seeds[j]);
-						seedList.get(seedList.size()-1).makeRange();
-						first=true;
+						first = false;
+					} else {
+						seedList.get(seedList.size() - 1).rangeSize = Long.parseLong(seeds[j]);
+						seedList.get(seedList.size() - 1).makeRange();
+						first = true;
 					}
 				}
-			}else{
+			} else {
 				if (line.contains("-to-")) {
-					//seed-to-soil map:
+					// seed-to-soil map:
 					String from = line.split("-to-")[0];
 					String to = line.split("-to-")[1].split(" ")[0];
 					/* ep.p(from +"->" + to); */
@@ -52,210 +50,157 @@ public class Solution_2_Day_05 {
 					Category c = new Category(from, to);
 					categoryList.add(c);
 
+				} else if (line.matches("[\\d]* [\\d*]* [\\d]*")) {
+					// numbers
+					// add then to the last category in the list (their category will always be the
+					// last one in the list)
 
-				} else if (line.matches("[\\d]* [\\d*]* [\\d]*")){
-					//numbers 
-					//add then to the last category in the list (their category will always be the last one in the list)
-					
 					long n1 = Long.parseLong(line.split(" ")[0]);
 					long n2 = Long.parseLong(line.split(" ")[1]);
 					long n3 = Long.parseLong(line.split(" ")[2]);
 					/* ep.p(n1 +"-"+ n2 +"-"+ n3); */
 					Range r = new Range(n2, n1, n3);
-					categoryList.get(categoryList.size()-1).ranges.add(r);
+					categoryList.get(categoryList.size() - 1).ranges.add(r);
 				}
 			}
 
-        }
-		
-		/* 
-		now for test print
-		seedList
-		categoryList 
-		ranges in category
-		*/
-		
-		/* seedList.forEach(s -> s.readRecord());
-		ep.p("");
-		categoryList.forEach(cl -> {cl.printCategory(); ep.p("");}); */
-		
+		}
 
-		// for every seed go through every category and if in range change number else dont
-
-		/*
-		Seed s;
-		long x;
-		Seed sA;
-		Seed sB;
-		Seed sC;
-		*/
-		//for debugging moved to the global variable |
-		//TempData td = new TempData();			   <-
-		//can 't remove or add in forEach - rewrite to iterator or for loop?
-		seedList.forEach(s -> s.readRecord());
-		ep.p("");	
+		
 		categoryList.forEach(cl -> {
+
 			for (int i = 0; i < seedList.size(); i++) {
 				td.j = 0;
 				for (Range r : cl.ranges) {
-					if(cl.name.equals("fertilizer")){
-						//ep.p("hi bug!");
-					}
+
 					td.i = i;
 					td.s = seedList.get(td.i);
-					if(cl.name.equals("fertilizer") && td.s.rangeStart==1787644422){
-						ep.p("here!");
-					}
-					if(td.s.state.equals(cl.from)){
+
+					if (td.s.state.equals(cl.from)) {
 						// mimo range vlevo
-						//   70-?      
-						//   	  90 - 100
+						// 70-?
+						// 90 - 100
 						// menší vlevo
-						if(td.s.rangeStart < r.sourceRangeStart){
-							// menší i vpravo
-							if(td.s.rangeEnd < r.sourceRangeStart){
-								//   79 - 92
-								//            98 - 99
-								// nechat stejné, změnit status
-								
-							}
-							//vpravo uprostřed
-							if(td.s.rangeEnd >= r.sourceRangeStart && td.s.rangeEnd <= r.sourceRangeEnd){
-								//   79 -       99
-								//           98 - 100
-								//rozlomit na 2 seedy 
+						if (td.s.rangeStart < r.sourceRangeStart) {
+
+							// vpravo uprostřed
+							if (td.s.rangeEnd >= r.sourceRangeStart && td.s.rangeEnd <= r.sourceRangeEnd) {
+								// 79 - 99
+								// 98 - 100
+								// rozlomit na 2 seedy
 								// 79-97; 98-99
-								//			^-zvětšit
-								td.x = r.destinationRange-r.sourceRangeStart; 
-								td.sA = new Seed(cl.name, td.s.rangeStart, r.sourceRangeStart-1);
-								td.sB = new Seed(cl.name, (r.sourceRangeStart + td.x) , (td.s.rangeEnd+td.x));
+								// ^-zvětšit
+								td.x = r.destinationRange - r.sourceRangeStart;
+								td.sA = new Seed(cl.name, td.s.rangeStart, r.sourceRangeStart - 1);
+								td.sB = new Seed(cl.name, (r.sourceRangeStart + td.x), (td.s.rangeEnd + td.x));
 								seedList.remove(td.i);
-								i-=1;
+								i -= 1;
 								seedList.add(td.sA);
 								seedList.add(td.sB);
 								break;
-							}
-							// vpravo větší
-							if(td.s.rangeEnd > r.sourceRangeEnd){
-								//   79 -                110
-								//           98 - 100
-								//rozlomit na 3 seedy 
+							}else if (td.s.rangeEnd > r.sourceRangeEnd) {
+								// vpravo větší
+								// 79 - 110
+								// 98 - 100
+								// rozlomit na 3 seedy
 								// 79-97; 98-100; 101 - 110
-								// 			^-zvětšit
-								td.x = r.destinationRange-r.sourceRangeStart;
-								td.sA = new Seed(cl.name, td.s.rangeStart, r.sourceRangeStart-1);
+								// ^-zvětšit
+								td.x = r.destinationRange - r.sourceRangeStart;
+								td.sA = new Seed(cl.name, td.s.rangeStart, r.sourceRangeStart - 1);
 								td.sB = new Seed(cl.name, (r.sourceRangeStart + td.x), (r.sourceRangeEnd + td.x));
-								td.sC = new Seed(cl.name, r.sourceRangeEnd+1 , td.s.rangeEnd);
+								td.sC = new Seed(cl.name, r.sourceRangeEnd + 1, td.s.rangeEnd);
 								seedList.remove(td.i);
-								i-=1;
+								i -= 1;
 								seedList.add(td.sA);
 								seedList.add(td.sB);
 								seedList.add(td.sC);
 								break;
+							}else if (td.s.rangeEnd < r.sourceRangeStart) {
+								// menší i vpravo
+								// 79 - 92
+								// 98 - 99
+								// nechat stejné, změnit status
+
 							}
-
-						}
-						// mimo range vpravo
-						//          90 - 100
-						//   70-80
-						if(td.s.rangeStart > r.sourceRangeStart && td.s.rangeStart > r.sourceRangeEnd){
 							
-							
-						}
 
-						// Levá strana v hranicích range
-						// 70-   100
-						//    80-   100
-						// 70-80
-						if(td.s.rangeStart >= r.sourceRangeStart && td.s.rangeStart <=r.sourceRangeEnd){
-							// test second side
-							if(td.s.rangeEnd <= r.sourceRangeEnd){
-								//     90 - 100
-								//   80   - 100
+						}else if (td.s.rangeStart >= r.sourceRangeStart && td.s.rangeStart <= r.sourceRangeEnd) {
+							// Levá strana v hranicích range
+							// 70- 100
+							// 80- 100
+							// 70-80
+							if (td.s.rangeEnd > r.sourceRangeEnd) {
+								// 90 - 110
+								// 80 - 100
+								// rozlomit na 2 seedy
+								// 90-100; 101-110
+								// ^-zvětšit
+								td.x = r.destinationRange - r.sourceRangeStart;
+								td.sA = new Seed(cl.name, (td.s.rangeStart + td.x), (r.sourceRangeEnd + td.x));
+								td.sB = new Seed(cl.name, r.sourceRangeEnd + 1, td.s.rangeEnd);
+								seedList.remove(td.i);
+								i -= 1;
+								seedList.add(td.sA);
+								seedList.add(td.sB);
+								break;
+
+							}else if (td.s.rangeEnd <= r.sourceRangeEnd) {
+								// test second side
+								// 90 - 100
+								// 80 - 100
 								// 1 seed zvětšit
-								td.x = r.destinationRange-r.sourceRangeStart;
+								td.x = r.destinationRange - r.sourceRangeStart;
 								td.s.changeRange(td.x);
 								td.s.changeState(cl.name);
 								break;
 							}
 
-							if(td.s.rangeEnd > r.sourceRangeEnd){
-								//     90 -    110
-								//   80   - 100
-								//rozlomit na 2 seedy
-								// 90-100; 101-110
-								//	 ^-zvětšit
-								td.x = r.destinationRange-r.sourceRangeStart; 
-								td.sA = new Seed(cl.name, (td.s.rangeStart+td.x), (r.sourceRangeEnd+td.x));
-								td.sB = new Seed(cl.name, r.sourceRangeEnd + 1, td.s.rangeEnd);
-								seedList.remove(td.i);
-								i-=1;
-								seedList.add(td.sA);
-								seedList.add(td.sB);
-								break;
+							
 
-							}
-
-						
-
+						}else if (td.s.rangeStart > r.sourceRangeStart && td.s.rangeStart > r.sourceRangeEnd) {
+							// mimo range vpravo
+							//      90 - 100
+							// 70-80
 						}
+						
 						if (td.j == cl.ranges.size() - 1) {
 							td.s.changeState(cl.name);
 						}
 						td.j++;
 
-					}else{
+					} else {
 						break;
 					}
-				
+
 				}
-				
-				
-			
 
 			}
-			//seedList.forEach(s -> s.readRecord());
-			//ep.p("");	
-			//GREEN - if seed range fits all on both sides
-			//if seed range doesnt't fit on both sides
-		//oldcode
-				/* if(s.number>= r.sourceRangeStart && ((r.rangeLength-1)+r.sourceRangeStart) >=s.number){
-					//destination is bigger or smaller than source range start
-					long x;
-					x = r.destinationRange-r.sourceRangeStart;
-					s.number += x; 
-					break;
-				}
-			}
-			s.makeRecord(cl.name,s.number); */
-		// end of old code
-		seedList.forEach(s -> s.readRecord());
-		ep.p("");
+			
 		});
+
 		
-		//seedList.forEach(s -> s.readRecord());
 		ArrayList<Long> seedRangeMin = new ArrayList<>();
 		seedList.forEach(currSeed -> {
-			seedRangeMin.add(currSeed.rangeStart); 
+			seedRangeMin.add(currSeed.rangeStart);
 		});
-		long minimum = seedRangeMin.get(0); 
-        for (int i = 1; i < seedRangeMin.size(); i++) { 
-            if (minimum > seedRangeMin.get(i)) 
-                minimum = seedRangeMin.get(i); 
-        }
+		long minimum = seedRangeMin.get(0);
+		for (int i = 1; i < seedRangeMin.size(); i++) {
+			if (minimum > seedRangeMin.get(i))
+				minimum = seedRangeMin.get(i);
+		}
 		seedList.clear();
 		return solution = minimum;
 	}
 
 	class Seed {
-		//changing state based on where the seed is currently located e.g. if seed -> soil state:soil 
+		// changing state based on where the seed is currently located e.g. if seed ->
+		// soil state:soil
 		String state;
 		long number;
 
 		long rangeStart;
 		long rangeEnd;
 		long rangeSize;
-
 
 		public Seed(String state, long number) {
 			this.state = state;
@@ -269,30 +214,30 @@ public class Solution_2_Day_05 {
 			this.rangeEnd = rangeEnd;
 		}
 
-		void makeRecord(String state, long number){
+		void makeRecord(String state, long number) {
 			this.state = state;
 			this.number = number;
 			this.rangeStart = number;
 		}
 
-		void changeState(String state){
+		void changeState(String state) {
 			this.state = state;
 		}
 
-		void makeRange(){
+		void makeRange() {
 			this.rangeEnd = (this.rangeStart + rangeSize) - 1;
 		}
 
-		void changeRange(long destVal){
+		void changeRange(long destVal) {
 			this.rangeStart += destVal;
-			this.rangeEnd += destVal; 
+			this.rangeEnd += destVal;
 		}
 
-		void readRecord(){
-			ep.p("state: " + state + "; range: <"+ rangeStart + "-" +rangeEnd+">");
+		void readRecord() {
+			ep.p("state: " + state + "; range: <" + rangeStart + "-" + rangeEnd + ">");
 		}
 
-	} 
+	}
 
 	class Category {
 		String from;
@@ -305,24 +250,24 @@ public class Solution_2_Day_05 {
 			this.ranges = new ArrayList<Range>();
 		}
 
-		void printCategory(){
-				ep.p(from+"-to-"+name +" map:"); 
-				readRanges();
+		void printCategory() {
+			ep.p(from + "-to-" + name + " map:");
+			readRanges();
 		}
 
-		void readRanges(){
+		void readRanges() {
 			for (Range r : ranges) {
 				r.printRange();
-			  }
+			}
 		}
 	}
 
 	class Range {
 		// 50 98 2
-		long sourceRangeStart; //98
+		long sourceRangeStart; // 98
 		long sourceRangeEnd; // (98+2) - 1
-		long destinationRange; //50
-		long rangeLength; //2
+		long destinationRange; // 50
+		long rangeLength; // 2
 
 		public Range(long sourceRangeStart, long destinationRange, long rangeLength) {
 			this.sourceRangeStart = sourceRangeStart;
@@ -331,12 +276,12 @@ public class Solution_2_Day_05 {
 			this.sourceRangeEnd = (sourceRangeStart + rangeLength) - 1;
 		}
 
-		void printRange(){
-			ep.p(destinationRange +" "+ sourceRangeStart +" "+rangeLength);
+		void printRange() {
+			ep.p(destinationRange + " " + sourceRangeStart + " " + rangeLength);
 		}
 	}
 
-	class TempData{
+	class TempData {
 		int j;
 		int i;
 		Seed s;
@@ -346,5 +291,5 @@ public class Solution_2_Day_05 {
 		Seed sC;
 	}
 	// ----debug zone----
-	
+
 }
