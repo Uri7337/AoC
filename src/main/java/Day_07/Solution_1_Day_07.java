7cf00fdd9946dd4aaf3ace11b94a8d0222fb2b30
Uri7337 <ondrasekuriasek@gmail.com>
2024-10-28 15:33:59 +0100

    Day_07 progress

--- src/main/java/Day_07/Solution_1_Day_07.java
+++ src/main/java/Day_07/Solution_1_Day_07.java
@@ -6,32 +6,41 @@ import shared.EasyPrint;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 public class Solution_1_Day_07 {
 	EasyPrint ep = new EasyPrint();
 
 	Object solution = "Nothing yet!";
-	
+
+	HashMap<String, Integer> pokerCards = new HashMap<String, Integer>();
+
 	ArrayList<String> file;
+
 	public Object getSolution(String filepath) {
-		
+
 		ReadFile rf = new ReadFile();
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
-			
-		
-		//Insert Solution Here:
+
+		// prepare poker cards
+		pokerCards.put("A", 14);
+		pokerCards.put("K", 13);
+		pokerCards.put("Q", 12);
+		pokerCards.put("J", 11);
+		pokerCards.put("T", 10);
+
+		for (int i = 2; i < 10; i++) {
+			pokerCards.put(i + "", i);
+		}
+
+		ep.p(pokerCards);
+		// Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
 
 			
-
+		}
 		return solution;
+		// ----debug zone----
+		
 	}
-
-	
-
-	
-	// ----debug zone----
-	
 }
--- src/main/java/Day_07/Solution_2_Day_07.java
+++ src/main/java/Day_07/Solution_2_Day_07.java
@@ -6,32 +6,27 @@ import shared.EasyPrint;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 public class Solution_2_Day_07 {
 	EasyPrint ep = new EasyPrint();
 
 	Object solution = "Nothing yet!";
-	
+
 	ArrayList<String> file;
+
 	public Object getSolution(String filepath) {
-		
+
 		ReadFile rf = new ReadFile();
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
-			
-		
-		//Insert Solution Here:
+
+		// Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
 
-			
+			return solution;
+		}
 
-		return solution;
+		// ----debug zone----
+
 	}
-
-	
-
-	
-	// ----debug zone----
-	
 }
