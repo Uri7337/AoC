d1f4e82d430d51a3e40631bd4e364a4af4cf74df
ourie <ourie@ourienb23.ahasw.cz>
2025-06-24 08:07:07 +0200

    Day_04 prep

--- src/main/java/Day_04/Day_04.java
+++ src/main/java/Day_04/Day_04.java
@@ -8,20 +8,29 @@ public class Day_04 {
         Solution_1_Day_04 s1 = new Solution_1_Day_04();
         Solution_2_Day_04 s2 = new Solution_2_Day_04();
 
-        String filepath_test = "./src/main/resources/Day_04_test_2023.txt";
-		String filepath = "./src/main/resources/Day_04_2023.txt";
+        String filepath_test = "./src/main/resources/Day_04_test_2024.txt";
+		String filepath = "./src/main/resources/Day_04_2024.txt";
+
+        String sol1 = "";
+        String sol2 = "";
+        boolean prod = false;
         
         ep.p("==Start==");
         ep.p("Solution *");
         ep.p("==Test prints==");
         String sol1_test = s1.getSolution(filepath_test).toString();
-        ep.p("\n==Answer prints==");
-        String sol1 = s1.getSolution(filepath).toString();
+        if(prod){
+            ep.p("\n==Answer prints==");
+            sol1 = s1.getSolution(filepath).toString();
+        }
+        ep.p("");
         ep.p("Solution **");
         ep.p("\n==Test prints==");
         String sol2_test = s2.getSolution(filepath_test).toString();
-        ep.p("\n==Answer prints==");
-        String sol2 = s2.getSolution(filepath).toString();
+        if(prod){
+            ep.p("\n==Answer prints==");
+            sol2 = s2.getSolution(filepath).toString();
+        }
         ep.p("\n==End==");
         
         ep.p("\n");
@@ -29,10 +38,15 @@ public class Day_04 {
         ep.p("===Solutions===");
         ep.p("Solution *");
         ep.p("Tests: " + sol1_test);
-        ep.p("Answer: " + sol1);
+        if(prod){
+            ep.p("Answer: " + sol1);
+        }
         ep.p("");
         ep.p("Solution **");
         ep.p("Tests: " + sol2_test);
-        ep.p("Answer: " + sol2);
+        if(prod){
+            ep.p("Answer: " + sol2);
+        }
+        
     }
 }
