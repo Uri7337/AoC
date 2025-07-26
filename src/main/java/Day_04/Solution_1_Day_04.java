bde2b34935e669f3bbe9b89adb744f579131331c
Uri <o.urie@seznam.cz>
2025-07-27 00:29:26 +0200

    progress

--- src/main/java/Day_04/Solution_1_Day_04.java
+++ src/main/java/Day_04/Solution_1_Day_04.java
@@ -19,12 +19,48 @@ public class Solution_1_Day_04 {
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
 
-		int a, b, c, d, dir;
-		a = -1;
-		b = file.get(0).length();
-		c = file.size();
-		d = -1;
-		dir = 0; // 0-7
+		final char[][] map = new char[file.size()][file.get(0).length()];
+		// changing the map to 2 dim array[][]
+		for (int i = 0; i < file.size(); i++) {
+			char[] tempArr = file.get(i).toCharArray();
+			for (int j = 0; j < tempArr.length; j++) {
+				map[i][j] = tempArr[j];
+			}
+		}
+
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				char currChar = map[i][j];
+				if (currChar == 'X') {
+					result += goAroundAndFindXMAS(j, i, map);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	int goAroundAndFindXMAS(int x, int y, char[][] map) {
+		int xmasCounter = 0;
+
+		// for loop around X (checks all directions)
+		for (int dir = 0; dir < 8; dir++) {
+			if (findXMAS(dir, x, y, 'M', map)) {
+				xmasCounter++;
+			}
+		}
+
+		return xmasCounter;
+	}
+
+	boolean findXMAS(int dir, int x, int y, char part, char[][] map) {
+
+		final int a, b, c, d;
+
+		a = 0; 			// Hranice - Horní Strana Mapy
+		b = map[0].length-1; 	// Hranice - Pravá Strana Mapy
+		c = map.length-1; 	// Hranice - Spodní Strana Mapy
+		d = 0;				// Hranice - Levá Strana Mapy
 
 		// x = 0
 		int[] x0 = { 3, 4, 5 };
@@ -35,34 +71,86 @@ public class Solution_1_Day_04 {
 		// x = b
 		int[] xb = { 0, 1, 7 };
 
-		char[][] map = new char[c][b];
-		// x y
-		// changing the map to 2 dim array[][]
-		for (int i = 0; i < c; i++) {
-			char[] tempArr = file.get(i).toCharArray();
-			for (int j = 0; j < tempArr.length; j++) {
-				map[i][j] = tempArr[j];
-			}
-		}
-
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				char currChar = map[i][j];
-				if(currChar == 'X'){
-					result += goAroundAndFindXMAS();
+		switch (dir) {
+			case 0: // Směr - Doprava →
+				if (x==b) {
+					return false;
+				}else{
+					x++;
 				}
-			}
+				break;
+
+			case 1: // Směr - Doprava Dolů ↘
+				if (x==b || y==c) {
+					return false;
+				}else{
+					x++;
+					y++;
+				}
+				break;
+
+			case 2: // Směr - Dolů ↓
+				if (y==c) {
+					return false;
+				}else{
+					y++;
+				}
+				break;
+
+			case 3: // Směr - Doleva Dolů ↙
+				if (x==d || y==c) {
+					return false;
+				}else{
+					x--;
+					y++;
+				}
+				break;
+
+			case 4: // Směr - Doleva ←
+				if (x==d) {
+					return false;
+				}else{
+					x--;
+				}
+				break;
+
+			case 5: // Směr - Doleva Nahoru ↖
+				if (x==d || y==c) {
+					return false;
+				}else{
+					x--;
+					y++;
+				}
+				break;
+
+			case 6: // Směr - Nahoru ↑
+
+				break;
+
+			case 7: // Směr - Doprava Nahoru ↗
+
+				break;
 		}
 
-		return result;
-	}
+		switch (part) {
+			// case 'X':
 
-	int goAroundAndFindXMAS(){
-		int xmasCounter = 0;
+			// break;
 
-		
+			case 'M':
 
-		return xmasCounter;
+				break;
+
+			case 'A':
+
+				break;
+
+			case 'S':
+
+				break;
+		}
+
+		return false;
 	}
 
 	boolean arrContains(int[] arr, int num) {
