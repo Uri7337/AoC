e557bb1954c1134c0cf03eb0cf95efa890180d62
uri <uri@uri.cz>
2025-08-10 12:18:00 +0200

    looks fine Day_04_2

--- src/main/java/Day_04/Solution_2_Day_04.java
+++ src/main/java/Day_04/Solution_2_Day_04.java
@@ -1,9 +1,9 @@
 package Day_04;
 
-import shared.ReadFile;
-import shared.EasyPrint;
-
 import java.util.ArrayList;
+
+import shared.EasyPrint;
+import shared.ReadFile;
 
 public class Solution_2_Day_04 {
 	EasyPrint ep = new EasyPrint();
@@ -42,18 +42,23 @@ public class Solution_2_Day_04 {
 
 	int goAroundAndFindMAS(int x, int y, char[][] map) {
 		
+		Xmap A = new Xmap(
+			loadParts(0, x, y, map),
+			loadParts(1, x, y, map),
+			loadParts(2, x, y, map),
+			loadParts(3, x, y, map)
+		);
 
-		// for loop around X (checks all directions)
-		for (int dir = 0; dir < 4; dir++) {
-			if (findXMAS(dir, x, y, 'M', map)) {
-				return 1;
-			}
-		}
+		if(A.containsDot()){ return 0;}
+		
+		//A.printA();
+		
+		if(A.isCorrect()){return 1;}
 
 		return 0;
 	}
 
-	boolean findXMAS(int dir, int x, int y, char part, char[][] map) {
+	char loadParts(int dir, int x, int y, char[][] map) {
 
 		final int a, b, c, d;
 
@@ -72,64 +77,109 @@ public class Solution_2_Day_04 {
 		//int[] xb = { 0, 1, 7 };
 
 		switch (dir) {
-			case 0: // Směr - Doprava Dolů ↘
-				if (x==b || y==c) {
-					return false;
-				}else{
-					x++;
-					y++;
-				}
-				break;
-
-			case 1: // Směr - Doleva Dolů ↙
-				if (x==d || y==c) {
-					return false;
-				}else{
-					x--;
-					y++;
-				}
-				break;
-
-			case 2: // Směr - Doleva Nahoru ↖
+			case 0: // Směr - Doleva Nahoru ↖
 				if (x==d || y==a) {
-					return false;
+					return '.';
 				}else{
 					x--;
 					y--;
+					return map[y][x];
 				}
-				break;
+				
 
-			case 3: // Směr - Doprava Nahoru ↗
+			case 1: // Směr - Doprava Nahoru ↗
 				if (x==b || y==a) {
-					return false;
+					return '.';
 				}else{
 					x++;
 					y--;
+					return map[y][x];
 				}
-				break;
-		}
-
-		switch (part) {
-			case 'M':
-				if(map[y][x]==part){
-					return findXMAS( dir,  x,  y,  'S',  map);
+			
+			case 2: // Směr - Doleva Dolů ↙
+				if (x==d || y==c) {
+					return '.';
 				}else{
-					return false;
+					x--;
+					y++;
+					return map[y][x];
 				}
 
-			case 'S':
-				if(map[y][x]==part){
-					return findXMAS( dir,  x,  y,  'M',  map);
+			case 3: // Směr - Doprava Dolů ↘
+				if (x==b || y==c) {
+					return '.';
 				}else{
-					return false;
+					x++;
+					y++;
+					return map[y][x];
 				}
-		}
+				
 
-		return false;
+						
+		}
+		return '.';
 	}
 
 	
 
 	// ----debug zone----
+
+}
+
+class Xmap{
+	char ul = '.';
+	char ur = '.';
+	char dl = '.';
+	char dr = '.';
+	int num = 0;
+
+    public Xmap(char ul, char ur, char dl, char dr) {
+		this.ul = ul;
+		this.ur = ur;
+		this.dl = dl;
+		this.dr = dr;	
+    }
+
+	boolean containsDot(){
+		return ul == '.' || ur == '.' || dl == '.' || dr == '.';
+	}
+
+	void printA(){
+		System.out.println(ul+"."+ur);
+		System.out.println(".A.");
+		System.out.println(dl+"."+dr);
+		System.out.println("isCorrect: "+this.isCorrect() +" " +this.num);
+	}
+
+	boolean isCorrect(){
+		if(this.ul =='M'){
+			if(this.ur == 'S'){
+				//3
+				//num = 3;
+				return this.dl == 'M' && this.dr == 'S';
+
+			}else if (this.ur == 'M') {
+				//2
+				//num= 2;
+				return this.dl == 'S' && this.dr == 'S';
+			}
+
+		}else if (this.ul =='S') {
+			//1,4
+			if(this.ur == 'S'){
+				//4
+				//num = 4;
+				return this.dl == 'M' && this.dr == 'M';
+			}else if (this.ur == 'M') {
+				//1
+				//num = 1;
+				return this.dl == 'S' && this.dr == 'M';
+			}
+			
+		}
+
+		return false;
+	}
+
 
 }
