d5017ca0c10000ac2cca3c6163a448de4dcb9e24
uri <uri@uri.cz>
2025-08-11 06:33:15 +0200

    variables arrangement

--- src/main/java/Day_05/Solution_1_Day_05.java
+++ src/main/java/Day_05/Solution_1_Day_05.java
@@ -18,13 +18,32 @@ public class Solution_1_Day_05 {
 		file = new ArrayList<String>();
 		file = rf.getInput(filepath);
 			
-		
+		ArrayList<Integer> first = new ArrayList<>();
+		ArrayList<Integer> second = new ArrayList<>();
+		ArrayList<String> list = new ArrayList<>();
+
 		//Insert Solution Here:
 		for (int i = 0; i < file.size(); i++) {
 			String line = file.get(i);
 
+			
+			if(line.contains("|")){
+				String[] temp = line.split("\\|");
+				first.add(Integer.parseInt(temp[0]));
+				second.add(Integer.parseInt(temp[1]));
+
+			}else if (line.contains(",")) {
+				list.add(line);
+
+			}
+				
+			
 
 		}
+		ep.p(first);
+		ep.p(second);
+		ep.p(list);
+
 		return result;
 	}
 	// ----debug zone----
