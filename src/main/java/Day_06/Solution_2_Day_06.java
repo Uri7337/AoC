0c148b7b4d7074a45f8a64f58417228eab120495
uri <uri@uri.cz>
2026-01-18 15:34:15 +0100

    Day_06 progress

--- src/main/java/Day_06/Solution_2_Day_06.java
+++ src/main/java/Day_06/Solution_2_Day_06.java
@@ -1,7 +1,5 @@
 package Day_06;
 
-
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -11,7 +9,7 @@ import shared.ReadFile;
 
 public class Solution_2_Day_06 {
 	EasyPrint ep = new EasyPrint();
-	
+
 	ArrayList<String> file;
 
 	long grandTotal;
@@ -20,43 +18,44 @@ public class Solution_2_Day_06 {
 		int id;
 		ArrayList<String> nums = new ArrayList<>();
 		String symbol; // + *
-		long res = 0; 
+		long res = 0;
 
-		Pillar(int id){
+		Pillar(int id) {
 			this.id = id;
 		}
 
-		void addmult(){
-			if(symbol.equals("*")){
-				this.res =1;
-			} 
+		void addmult() {
+			if (symbol.equals("*")) {
+				this.res = 1;
+			}
 			for (int i = 0; i < nums.size(); i++) {
 				switch (symbol) {
 
 					case "+" -> {
 						res += Long.valueOf(nums.get(i));
-                    }
-						
+					}
+
 					case "*" -> {
 						res *= Long.valueOf(nums.get(i));
-                    }
+					}
 
 					default -> ep.p(symbol);
 				}
 			}
 		}
 
-		void showNums(){
+		void showNums() {
 			for (int i = 0; i < nums.size(); i++) {
 				ep.np(nums.get(i));
-				if(i<nums.size()-1)ep.np(" "+symbol+" ");
-				if(i==nums.size()-1)ep.np(" = " +this.res);
+				if (i < nums.size() - 1)
+					ep.np(" " + symbol + " ");
+				if (i == nums.size() - 1)
+					ep.np(" = " + this.res);
 			}
 			ep.p("");
 		}
 
 	}
-
 
 	public Object getSolution(String filepath) {
 
@@ -75,50 +74,43 @@ public class Solution_2_Day_06 {
 
 			for (int i = 0; i < sarr.length; i++) {
 				String singleColumn = sarr[i].strip();
-
-				if(!singleColumn.equals("")){
+				ep.p(singleColumn);
+				if (!singleColumn.equals("")) {
 					String[] singleDigit = singleColumn.split("");
 					for (int j = 0; j < singleDigit.length; j++) {
-						ep.p(singleDigit[j]);
+						// ep.p(singleDigit[j]);
 						linePieces.add(singleDigit[j]);
 					}
-					
+
 				}
 			}
 
 			for (int i = 0; i < linePieces.size(); i++) {
 				String linePiece = linePieces.get(i);
-				//ep.p(linePiece);
-				if(pillars.get(i)==null){
+				// ep.p(linePiece);
+				if (pillars.get(i) == null) {
 					Pillar p = new Pillar(i);
 					pillars.put(i, p);
 				}
 
-				if(linePiece.equals("*") || linePiece.equals("+") ){
-						pillars.get(i).symbol = linePiece;
-				}else{
+				if (linePiece.equals("*") || linePiece.equals("+")) {
+					pillars.get(i).symbol = linePiece;
+				} else {
 					pillars.get(i).nums.add(linePiece);
 				}
 			}
-			
-						
-			
-			
+
 		}
 
 		for (int i = 0; i < pillars.size(); i++) {
 			Pillar p = pillars.get(i);
-			//p.addmult();
-			p.showNums();
+			// p.addmult();
+			// p.showNums();
 			grandTotal += p.res;
 		}
 
-	
-
-
 		
-		//4771265398012
-        return grandTotal;
-    }
-	
+		return grandTotal;
+	}
+
 }
