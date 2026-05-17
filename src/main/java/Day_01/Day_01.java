06ca97afe9022d5c774660f5bfea860da096d494
uri <uri@uri.cz>
2026-05-17 22:01:45 +0200

    Day_01 prep

--- src/main/java/Day_01/Day_01.java
+++ src/main/java/Day_01/Day_01.java
@@ -1,31 +1,40 @@
 package Day_01;
 
 import shared.EasyPrint;
-public class Day_01 {
+public class  {
 
     public static void main(String[] args) {
         EasyPrint ep = new EasyPrint();
         Solution_1_Day_01 s1 = new Solution_1_Day_01();
         Solution_2_Day_01 s2 = new Solution_2_Day_01();
 
+        String filepath_my_test = "./src/main/resources/Day_01_test_2021_1.txt";
         String filepath_test = "./src/main/resources/Day_01_test_2021.txt";
 		String filepath = "./src/main/resources/Day_01_2021.txt";
 
+        
         String sol1 = "";
         String sol2 = "";
 
+        String sol0_test = "";
         String sol1_test = "";
         String sol2_test = "";
 
-        boolean tests = false;
-        boolean prod = true;
+        boolean mytest = false;
+        boolean tests = true;
+        boolean prod = false;
         boolean part1 = false;
         boolean part2 = true;
-
         
         ep.p("==Start==");
         if(part1){
             ep.p("\nSolution *");
+            if(mytest){
+                ep.p("==myTest prints==");
+                sol0_test = s1.getSolution(filepath_my_test).toString();
+                ep.p("");
+            }
+
             if(tests){
                 ep.p("==Test prints==");
                 sol1_test = s1.getSolution(filepath_test).toString();
@@ -55,13 +64,19 @@ public class Day_01 {
         ep.p("===Solutions===");
         if(part1){
             ep.p("Solution *");
+            if (mytest) {
+                ep.p("myTests: " + sol0_test);
+                if(Long.valueOf(sol0_test) == 25){ep.p(" | OK: True");}else{ep.p(" | OK: False");}
+            }
+
              if(tests){
                 ep.p("Tests: " + sol1_test);
-                if(Integer.valueOf(sol1_test) == 3){ep.p("| OK: True");}else{ep.p(" | OK: False");}
+                if(Long.valueOf(sol1_test) == 21){ep.p(" | OK: True");}else{ep.p(" | OK: False");}
              }
             
             if(prod){
                 ep.p("Answer: " + sol1);
+                if(Long.valueOf(sol1) < 1518){ep.p(" | OK: Maybe");}else{ep.p(" | Too High :(");}
             }
         }
         ep.p("");
@@ -69,7 +84,7 @@ public class Day_01 {
             ep.p("Solution **");
              if(tests){
                 ep.np("Tests: " + sol2_test);
-                if(Integer.valueOf(sol2_test) == 6){ep.p("| OK: True");}else{ep.p(" | OK: False");}
+                if(Long.valueOf(sol2_test) == 40){ep.p(" | OK: True");}else{ep.p(" | OK: False");}
              }
             if(prod){
                 ep.p("Answer: " + sol2);
@@ -79,3 +94,6 @@ public class Day_01 {
         
     }
 }
+
+
+
--- src/main/java/Day_01/Solution_2_Day_01.java
+++ src/main/java/Day_01/Solution_2_Day_01.java
@@ -8,82 +8,31 @@ import shared.ReadFile;
 public class Solution_2_Day_01 {
 	EasyPrint ep = new EasyPrint();
 
-	ArrayList<String> rotations;
+	ArrayList<String> file;
+
+	long res;
+	
+
+
 
 	public Object getSolution(String filepath) {
 
-		ReadFile rf = new ReadFile();
-		rotations = new ArrayList<String>();
-		rotations = rf.getInput(filepath);
-
-		int dial = 50;
-		int password = 0;
+		res = 0;
 		
-		// rotations preparation
-		for (int y = 0; y < rotations.size(); y++) {
-			String line = rotations.get(y);
+		ReadFile rf = new ReadFile();
+		file = new ArrayList<String>();
+		file = rf.getInput(filepath);
 
-			char direction = line.charAt(0);
-			int rotation = Integer.parseInt(line.substring(1, line.length()));
+		
 
-			/* if (direction == 'R') {
-				if (dial == 0) {
-					password--;
-				}
-				dial += rotation;
-
-				while (100 <= dial) {
-					password++;
-					dial -= 100;
-				}
-			} else {
-				if (dial == 0) {
-					password--;
-				}
-				dial -= rotation;
-				while (0 > dial) {
-					password++;
-					dial += 100;
-
-				}
-			} */
-
+		for (int y = 0; y < file.size(); y++) {
+			String line = file.get(y);
 			
-			if (direction == 'R') {
-				for (int index = rotation; index > 0; index--) {
-					if (dial == 99) {
-						dial = 0;
-						password++;
-						
-					} else {
-						dial++;
-					}
-				}
-			} else {
-				for (int index = rotation; index > 0; index--) {
-					if (dial == 0) {
-						dial = 99;
-						if(index!=rotation)password++;
-						
-						
-					} else {
-						dial--;
-					}			
-				}
-				
-			}
-			if(dial==0 && direction!='R'){password++;}
-			ep.np(direction+""+rotation+" ");
-			ep.p(dial +" :"+ password);
-			
+			ep.p(line);
+        }
 
-		}
-
-		// 6889 - wrong
-		// 8047 - wrong
-		// 5748 - wrong
-		// 6860
-		return password;
+		ep.p(res);
+		return res;
 	}
 
 }
