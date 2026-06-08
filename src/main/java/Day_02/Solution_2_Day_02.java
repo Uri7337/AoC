482c7619849a11212830329f2a366de377a7f153
ourie <ourie@ourienb23.ahasw.cz>
2026-06-08 10:59:44 +0200

    Day_02 done

--- src/main/java/Day_02/Solution_2_Day_02.java
+++ src/main/java/Day_02/Solution_2_Day_02.java
@@ -23,34 +23,24 @@ public class Solution_2_Day_02 {
 		int horizontal = 0;
 		int depth = 0;
 		int aim = 0;
-		boolean zero = false;
+		
 
 		for (int y = 0; y < file.size(); y++) {
 			String[] commands = file.get(y).split(" ");
 
 			if (commands[0].equals("up")) {
-				//depth -= Integer.valueOf(commands[1]);
 				aim -= Integer.valueOf(commands[1]);
 			} else if (commands[0].equals("down")) {
-				//depth += Integer.valueOf(commands[1]);
 				aim += Integer.valueOf(commands[1]);
 			} else {
 				horizontal += Integer.valueOf(commands[1]);
-				if(aim==0){
-					aim++;
-					zero = true;
-				} 
 				depth = depth + (aim *Integer.valueOf(commands[1]));
-				if(zero){
-					aim--;
-					zero = false;
-				}
 			}
 
 		}
-		ep.p(depth);
+		
 		res = horizontal * depth;
-		ep.p(res);
+		
 		return res;
 	}
 
